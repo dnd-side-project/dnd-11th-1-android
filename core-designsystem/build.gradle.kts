74d260e131927f1dbf2a7de02afe1499be9f0717
uselessNaming <seohojon@naver.com>
2024-07-28 22:29:01 +0900

    Chore: material3 버전 정보 추가

--- core-designsystem/build.gradle.kts
+++ core-designsystem/build.gradle.kts
@@ -47,10 +47,13 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.material3)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.ui.tooling)
+    implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    debugImplementation(libs.androidx.ui.tooling)
 }
