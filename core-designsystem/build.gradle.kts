68b39d9873ad4a119bb53260bd427364eb74914e
uselessNaming <seohojon@naver.com>
2024-07-28 22:08:28 +0900

    Chore: androidx ui 의존성 추가

--- core-designsystem/build.gradle.kts
+++ core-designsystem/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
