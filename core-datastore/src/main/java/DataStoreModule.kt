72d245ca6bd1a69b02df1297a0d7f9c4b7ba8b98
uselessNaming <seohojon@naver.com>
2024-07-30 22:02:50 +0900

    Chore: 패키지 명 추가

--- core-datastore/src/main/java/DataStoreModule.kt
+++ core-datastore/src/main/java/DataStoreModule.kt
@@ -1,15 +1,17 @@
+package com.materip.core_datastore
+
 import com.materip.core_datastore.test_datastore.TestDataStore
 import com.materip.core_datastore.test_datastore.TestDataStoreImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
+import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
 interface DataStoreModule {
     @Singleton
     @Binds
-    fun provideTestDataStore(testDataStore: TestDataStoreImpl): TestDataStore
+    fun provideTestDataStore(testDataStoreImpl: TestDataStoreImpl): TestDataStore
 }
