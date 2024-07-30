1300134c282048c5d24fbdc258648144dc5120c5
상은 <71699054+vmkmym@users.noreply.github.com>
2024-07-30 17:37:53 +0900

    chore: 의존성 추가 및 빌드 옵션 추가
    - buildFeatures의 compose를 true로 설정해서 core-designsystem에서도 Preview를 볼 수 있게 함
    - composeBom의 버전을 nowinandroid 버전과 맞춤
    - meterial3의 버전을 1.2.1로 선언함

--- core-designsystem/build.gradle.kts
+++ core-designsystem/build.gradle.kts
@@ -30,14 +30,28 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1" // 1.5.14는 Kotlin 1.9.24를 타겟팅
+    }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 }
