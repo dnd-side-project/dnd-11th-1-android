f2e9aa7e71d4324d117a4dc0768c1063649e6f87
상은 <71699054+vmkmym@users.noreply.github.com>
2024-07-31 23:23:47 +0900

    refactor: OnboardingElevatedCard 리팩토링
    - 선택된 카드의 여부와 상태 관리를 부모 컴포저블에서 관리하게끔 코드 변경
    
    Resolves     : https://dnd-11-1-tripmate.atlassian.net/browse/ISSUE-75

--- core-designsystem/src/main/java/com/materip/matetrip/component/Card.kt
+++ core-designsystem/src/main/java/com/materip/matetrip/component/Card.kt
@@ -2,27 +2,19 @@ package com.materip.matetrip.component
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,33 +24,23 @@ import com.materip.matetrip.theme.MateTripTypography
 
 /**
  * Onboarding Card
- * text는 카드의 정보를 나타냄
- * icon은 카드의 아이콘을 나타냄
- * selectedCards는 선택된 카드의 text를 저장하는 리스트 // 이 부분은 변경 가능, 필요없으면 매개변수 삭제
  */
 
 @Composable
 fun OnboardingElevatedCard(
     text: String,
     icon: Int,
-    selectedCards: MutableList<String>
+    isSelected: Boolean, // 선택된 카드인지 여부, 이후는 부모 컴포저블에서 관리
+    onClick: () -> Unit // 선택된 카드의 상태관리는 부모 컴포저블에서 관리
 ) {
-    var isClicked by remember { mutableStateOf(false) }
-    val backgroundColor = if (isClicked) Color(0xFF3553F2) else Color(0xFFEEF0FF)
-    val textColor = if (isClicked) Color.White else MateTripTypography.bodyMedium.color
+    val backgroundColor = if (isSelected) Color(0xFF3553F2) else Color(0xFFEEF0FF)
+    val textColor = if (isSelected) Color.White else MateTripTypography.bodyMedium.color
 
     Card(
         colors = CardDefaults.cardColors(containerColor = backgroundColor),
         modifier = Modifier
             .size(145.dp)
-            .clickable {
-                isClicked = !isClicked
-                if (isClicked) {
-                    selectedCards.add(text)
-                } else {
-                    selectedCards.remove(text)
-                }
-            }
+            .clickable { onClick() }
     ) {
         Column {
             Text(
@@ -79,18 +61,25 @@ fun OnboardingElevatedCard(
     }
 }
 
-@Composable
-fun CardExample() {
-    val selectedCards = remember { mutableStateListOf<String>() }
-    // OnboardingElevatedCard를 호출하고 selectedCards에 selectedCards를 할당
-}
+
 
 @Preview
 @Composable
-fun OnboardingCardPreview() {
+fun OnboardingCardSelectionPreview() {
+    val selectedCards = remember { mutableStateListOf<String>() }
+
+    val toggleSelection: (String) -> Unit = { text ->
+        if (selectedCards.contains(text)) {
+            selectedCards.remove(text)
+        } else {
+            selectedCards.add(text)
+        }
+    }
+
     OnboardingElevatedCard(
         text = "남성",
         icon = man_icon,
-        selectedCards = remember { mutableListOf() }
+        isSelected = selectedCards.contains("남성"),
+        onClick = { toggleSelection("남성") }
     )
 }
