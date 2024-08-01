16bd45bfa6208067090af25025533770c9344b34
uselessNaming <seohojon@naver.com>
2024-08-01 11:51:12 +0900

    Design: 화면 구도 변경

--- feature-login/src/main/java/com/materip/feature_login/screens/LoginScreen.kt
+++ feature-login/src/main/java/com/materip/feature_login/screens/LoginScreen.kt
@@ -1,43 +1,113 @@
 package com.materip.feature_login
 
+import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.util.Utility
+import com.kakao.sdk.user.UserApiClient
 import com.materip.core_designsystem.CustomButton
+import com.materip.feature_login.view_models.LoginViewModel
 
 @Composable
-fun LoginRoute(){
-    LoginScreen()
+fun LoginRoute(
+    navOnBoarding: () -> Unit,
+    viewModel: LoginViewModel = hiltViewModel()
+){
+    val context = LocalContext.current
+    Log.d("AUTH LOGIN TEST", "key hash : ${Utility.getKeyHash(context)}")
+
+    LoginScreen(
+        isLogin = viewModel.isLogin.collectAsStateWithLifecycle().value,
+        doLogin = {viewModel.doLogin(context)},
+        navOnBoarding = navOnBoarding
+    )
 }
 
 @Composable
-fun LoginScreen(){
+fun LoginScreen(
+    isLogin: Boolean,
+    doLogin: () -> Unit,
+    navOnBoarding: () -> Unit,
+){
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(color = Color.White)
+            .padding(horizontal = 35.dp, vertical = 60.dp),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
     ){
+        Spacer(Modifier.height(54.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth()
+                .height(80.dp)
+                .padding(horizontal = 10.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ){
+            Text(
+                text = "MATE",
+                fontSize = 36.sp
+            )
+            Image(
+                imageVector = Icons.Filled.AddCircle,
+                contentDescription = "APP ICON" //app icon으로 대체
+            )
+            Text(
+                text = "TRIP",
+                fontSize = 36.sp
+            )
+        }
         CustomButton(
+            modifier = Modifier.fillMaxWidth()
+                .height(54.dp),
             containerColor = Color.Yellow,
             shape = RoundedCornerShape(size = 10.dp),
             text = "카카오 로그인",
             textColor = Color.Black,
             fontSize = 16.sp,
             isEnabled = true,
-            onClick = {/** 로그인 */}
+            onClick = doLogin
         )
+    }
+    LaunchedEffect(isLogin){
+        if (isLogin){
+            navOnBoarding()
+        }
     }
 }
 
 @Preview
 @Composable
 private fun UITest(){
-    LoginScreen()
+    LoginScreen(
+        doLogin = {},
+        isLogin = false,
+        navOnBoarding = {}
+    )
 }
