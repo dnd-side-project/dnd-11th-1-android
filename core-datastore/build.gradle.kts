c2f8ce0f1c23c129487e05fb84bcad3fae751372
uselessNaming <seohojon@naver.com>
2024-08-01 12:05:20 +0900

    Chore: database 모듈 추가

--- core-datastore/build.gradle.kts
+++ core-datastore/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation(project(":core-model"))
     implementation(project(":core-network"))
     implementation(project(":core-common"))
+    implementation(project(":core-database"))
 
     //hilt
     implementation(libs.bundles.hilt.impl)
