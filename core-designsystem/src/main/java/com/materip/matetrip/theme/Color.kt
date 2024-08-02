9fc9a5661c1e33fc53215777ee6e789d95821837
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-02 13:17:39 +0900

    design: 컬러 색상 추가

--- core-designsystem/src/main/java/com/materip/matetrip/theme/Color.kt
+++ core-designsystem/src/main/java/com/materip/matetrip/theme/Color.kt
@@ -1,5 +1,7 @@
 package com.materip.matetrip.theme
 
+import androidx.compose.ui.graphics.Color
+
 /**
  * 메이트립 Android Color
  */
@@ -7,3 +9,4 @@ package com.materip.matetrip.theme
 const val searchBarText = 0xFF939094
 const val SecondColor = 0xFFEEF0FF
 const val MainColor = 0xFF3553F2
+const val Gray05 = 0xFFAEAAAE
