335aab4a5b900f2ef04c25585085f83e02a8248a
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-02 13:20:29 +0900

    refactor: 변수 타입 수정

--- core-designsystem/src/main/java/com/materip/matetrip/component/ProgressIndicator.kt
+++ core-designsystem/src/main/java/com/materip/matetrip/component/ProgressIndicator.kt
@@ -4,11 +4,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
@@ -29,7 +30,7 @@ import kotlinx.coroutines.launch
 
 @Composable
 fun ProgressIndicator() {
-    var currentProgress by remember { mutableStateOf(0.25f) }
+    var currentProgress by remember { mutableFloatStateOf(0.25f) }
     val scope = rememberCoroutineScope()
 
     Column(
@@ -45,7 +46,7 @@ fun ProgressIndicator() {
             trackColor = Color(SecondColor),
             strokeCap = StrokeCap.Round,
         )
-        Spacer(modifier = Modifier.size(40.dp))
+        Spacer(modifier = Modifier.height(40.dp))
         MateTripButton(
             onClick = {
                 scope.launch {
