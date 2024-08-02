bbcbd3d8cc2b20dda27d4e3ab2a4cb833838d97a
uselessNaming <seohojon@naver.com>
2024-08-02 18:51:07 +0900

    Feat: 임시 로그인 로직 구현

--- feature-login/src/main/java/com/materip/feature_login/view_models/LoginViewModel.kt
+++ feature-login/src/main/java/com/materip/feature_login/view_models/LoginViewModel.kt
@@ -3,23 +3,28 @@ package com.materip.feature_login.view_models
 import android.content.Context
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.model.ClientError
 import com.kakao.sdk.common.model.ClientErrorCause
 import com.kakao.sdk.user.UserApiClient
+import com.materip.core_repository.repository.login_repository.LoginRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class LoginViewModel @Inject constructor() : ViewModel() {
+class LoginViewModel @Inject constructor(
+    private val loginRepository: LoginRepository
+) : ViewModel() {
     private val _isLogin = MutableStateFlow<Boolean>(false)
     val isLogin get() = _isLogin.asStateFlow()
-
     private val generalError = MutableStateFlow<Pair<Boolean, String?>>(Pair(false,null))
-    private val kakaoToken = MutableStateFlow<String?>(null)
 
     //login 함수
     fun doLogin(context: Context){
@@ -27,7 +32,7 @@ class LoginViewModel @Inject constructor() : ViewModel() {
             if (error != null) {
                 generalError.update{Pair(true, "KAKAO LOGIN FAILED")}
             } else if (token != null){
-                kakaoToken.update{ token.accessToken }
+                viewModelScope.launch{loginRepository.saveAuthToken(token.accessToken)}
                 _isLogin.update{ true }
             }
         }
@@ -40,7 +45,7 @@ class LoginViewModel @Inject constructor() : ViewModel() {
                     }
                     UserApiClient.instance.loginWithKakaoAccount(context, callback = loginCallBack)
                 } else if (token != null){
-                    kakaoToken.update{token.accessToken}
+                    viewModelScope.launch{loginRepository.saveAuthToken(token.accessToken)}
                     _isLogin.update{true}
                 }
             }
