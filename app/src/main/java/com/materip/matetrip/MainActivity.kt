e9cea50615af9ba571d0e2779bd038c5e07db6a2
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-04 17:46:56 +0900

    style: MainActivity에 코드 제거
    - 임시로 선언한 코드 제거

--- app/src/main/java/com/materip/matetrip/MainActivity.kt
+++ app/src/main/java/com/materip/matetrip/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MatetripTheme {
-                MateTripTopAppBar()
+
             }
         }
     }
