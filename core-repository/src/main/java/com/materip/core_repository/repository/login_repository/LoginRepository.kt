17dc01203e5b4e62d5a423d0c6b7366d20a5eb0c
uselessNaming <seohojon@naver.com>
2024-08-05 20:26:22 +0900

    Feat: kakao login 함수 추가

--- core-repository/src/main/java/com/materip/core_repository/repository/login_repository/LoginRepository.kt
+++ core-repository/src/main/java/com/materip/core_repository/repository/login_repository/LoginRepository.kt
@@ -1,5 +1,8 @@
 package com.materip.core_repository.repository.login_repository
 
+import com.materip.core_common.ResultResponse
+import com.materip.core_model.request.LoginRequestDto
+import com.materip.core_model.response.LoginResponseDto
 import kotlinx.coroutines.flow.Flow
 
 interface LoginRepository {
@@ -12,4 +15,5 @@ interface LoginRepository {
     suspend fun deleteAuthToken()
     suspend fun deleteRefreshToken()
     suspend fun deleteKakaoToken()
+    suspend fun loginKakao(requestDto: LoginRequestDto): ResultResponse<LoginResponseDto>
 }
