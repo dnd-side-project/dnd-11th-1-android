67c60a157b27992c521057246e4a153c4224eeca
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-06 02:10:31 +0900

    init: HomeScreen, HomeHiltViewModel

---
+++ feature-home/src/main/kotlin/com/materip/matetrip/viewModel/HomeHiltViewModel.kt
@@ -0,0 +1,11 @@
+package com.materip.matetrip.viewModel
+
+//@HiltViewModel
+//class HomeHiltViewModel @Inject constructor(
+//    private val repository: HomeRepository,
+//    private val homeUseCase: HomeUseCase,
+//    private val homeMapper: HomeMapper,
+//): ViewModel() {
+//    // ViewModel implementation
+//
+//}
