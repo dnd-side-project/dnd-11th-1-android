3a9bf081a64b72110f6bfee83e6c54bb5d3bfe92
uselessNaming <seohojon@naver.com>
2024-08-07 00:39:18 +0900

    Feat: post token 함수 추가

--- feature-login/src/main/java/com/materip/feature_login/view_models/LoginViewModel.kt
+++ feature-login/src/main/java/com/materip/feature_login/view_models/LoginViewModel.kt
@@ -8,11 +8,18 @@ import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.model.ClientError
 import com.kakao.sdk.common.model.ClientErrorCause
 import com.kakao.sdk.user.UserApiClient
+import com.materip.core_common.ErrorState
+import com.materip.core_model.request.LoginRequestDto
 import com.materip.core_repository.repository.login_repository.LoginRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.mapLatest
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
@@ -24,16 +31,23 @@ class LoginViewModel @Inject constructor(
 ) : ViewModel() {
     private val _isLogin = MutableStateFlow<Boolean>(false)
     val isLogin get() = _isLogin.asStateFlow()
-    private val generalError = MutableStateFlow<Pair<Boolean, String?>>(Pair(false,null))
+
+    private val generalErrorState = MutableStateFlow<Pair<Boolean, String>>(Pair(false, ""))
+    val errorState : StateFlow<ErrorState> = generalErrorState.mapLatest{
+        ErrorState.NoAuthError(it)
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5_000),
+        initialValue = ErrorState.Loading
+    )
 
     //login 함수
     fun doLogin(context: Context){
         val loginCallBack: (OAuthToken?, Throwable?) -> Unit = { token, error ->
             if (error != null) {
-                generalError.update{Pair(true, "KAKAO LOGIN FAILED")}
+                generalErrorState.update{Pair(true, "KAKAO LOGIN FAILED")}
             } else if (token != null){
-                viewModelScope.launch{loginRepository.saveAuthToken(token.accessToken)}
-                _isLogin.update{ true }
+                postToken(token.accessToken)
             }
         }
         //카카오톡 로그인
@@ -45,12 +59,26 @@ class LoginViewModel @Inject constructor(
                     }
                     UserApiClient.instance.loginWithKakaoAccount(context, callback = loginCallBack)
                 } else if (token != null){
-                    viewModelScope.launch{loginRepository.saveAuthToken(token.accessToken)}
-                    _isLogin.update{true}
+                    postToken(token.accessToken)
                 }
             }
         } else {
             UserApiClient.instance.loginWithKakaoAccount(context, callback = loginCallBack)
+        }
+    }
+
+    private fun postToken(kakaoToken: String){
+        viewModelScope.launch{
+            loginRepository.saveKakaoAccessToken(kakaoToken)
+            val requestDto = LoginRequestDto(provider = "KAKAO", accessToken = kakaoToken)
+            val result = loginRepository.loginKakao(requestDto)
+            if (result.error != null) {
+                generalErrorState.update { Pair(true, result.error!!.message) }
+                return@launch
+            }
+            loginRepository.saveAuthToken(result.data!!.accessToken)
+            loginRepository.saveRefreshToken(result.data!!.refreshToken)
+            _isLogin.update{ true }
         }
     }
 }
