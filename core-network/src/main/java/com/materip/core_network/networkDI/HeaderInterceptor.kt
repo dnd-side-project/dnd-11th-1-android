189d9fde947934700bf66857f6ef405e9a1c1ced
uselessNaming <seohojon@naver.com>
2024-08-07 01:15:05 +0900

    Feat: interceptor 추가

---
+++ core-network/src/main/java/com/materip/core_network/networkDI/HeaderInterceptor.kt
@@ -0,0 +1,23 @@
+package com.materip.core_network.networkDI
+
+import com.materip.core_database.TokenManager
+import kotlinx.coroutines.runBlocking
+import okhttp3.Interceptor
+import okhttp3.Response
+import javax.inject.Inject
+
+class HeaderInterceptor @Inject constructor(
+    private val tokenManager: TokenManager
+): Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val token = runBlocking{
+            tokenManager.getAuthTokenForHeader() ?: ""
+        }
+        val newRequest = chain.request().newBuilder()
+            .addHeader("X-AUTH-HEADER", token)
+            .build()
+        val response = chain.proceed(newRequest)
+
+        return response
+    }
+}
