36d0ffd818f2b7aaeefa24cd58e33c4d16ac0abb
uselessNaming <seohojon@naver.com>
2024-08-07 01:15:44 +0900

    Feat: interceptor, authenticator DI

--- core-network/src/main/java/com/materip/core_network/networkDI/NetworkModule.kt
+++ core-network/src/main/java/com/materip/core_network/networkDI/NetworkModule.kt
@@ -2,6 +2,8 @@ package com.materip.core_network
 
 import com.google.gson.GsonBuilder
 import com.materip.core_database.TokenManager
+import com.materip.core_network.networkDI.AuthAuthenticator
+import com.materip.core_network.networkDI.HeaderInterceptor
 import com.materip.core_network.service.login.LoginService
 import com.skydoves.sandwich.retrofit.adapters.ApiResponseCallAdapterFactory
 import dagger.Module
@@ -14,6 +16,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 import com.materip.core_network.service.test.TestService
 import kotlinx.coroutines.runBlocking
+import okhttp3.Authenticator
 import okhttp3.Interceptor
 import java.util.concurrent.TimeUnit
 
@@ -21,6 +24,16 @@ import java.util.concurrent.TimeUnit
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
+    @Singleton
+    @Provides
+    fun provideHeaderInterceptor(tokenManager: TokenManager): HeaderInterceptor {
+        return HeaderInterceptor(tokenManager)
+    }
+    @Singleton
+    @Provides
+    fun provideAuthInterceptor(tokenManager: TokenManager): Authenticator{
+        return AuthAuthenticator(tokenManager)
+    }
     @Singleton
     @Provides
     fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
@@ -34,26 +47,13 @@ object NetworkModule {
 
     @Singleton
     @Provides
-    fun provideOkHttpClient(headerInterceptor: Interceptor): OkHttpClient{
-        return OkHttpClient().newBuilder()
+    fun provideOkHttpClient(headerInterceptor: HeaderInterceptor, authInterceptor: AuthAuthenticator): OkHttpClient{
+        return OkHttpClient.Builder()
             .connectTimeout(60, TimeUnit.SECONDS)
             .readTimeout(60, TimeUnit.SECONDS)
             .addInterceptor(headerInterceptor)
+            .authenticator(authInterceptor)
             .build()
-    }
-
-    @Singleton
-    @Provides
-    fun provideHeaderInterceptor(tokenManager: TokenManager): Interceptor{
-        val token = runBlocking{ tokenManager.getAuthTokenForHeader() }
-        return Interceptor{chain ->
-            with(chain){
-                val newRequest = request().newBuilder()
-                    .header("X-AUTH-TOKEN", "${token}")
-                    .build()
-                proceed(newRequest)
-            }
-        }
     }
 
     @Singleton
