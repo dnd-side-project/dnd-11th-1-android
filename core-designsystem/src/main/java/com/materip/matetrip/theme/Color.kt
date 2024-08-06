7fa3a74c60aaf7e5fe866c112480e6bc0e3afb73
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-06 18:38:49 +0900

    style: Color.kt 코드 형식 수정
    - const val에서 internal val로 수정 후 Color(컬러코드) 할당하는 방식

--- core-designsystem/src/main/java/com/materip/matetrip/theme/Color.kt
+++ core-designsystem/src/main/java/com/materip/matetrip/theme/Color.kt
@@ -1,36 +1,30 @@
 package com.materip.matetrip.theme
 
+import androidx.compose.ui.graphics.Color
+
 /**
  * 메이트립 Android Color
  */
 // Main Color
-const val primary = 0xFF3553F2
-const val blue01 = 0xFF9BA1DA
-const val blue02 = 0xFFBEC3EF
-const val blue03 = 0xFFDDE1FC
-const val blue04 = 0xFFEEF0FF
+internal val Primary = Color(0xFF3553F2)
+internal val Blue_01 = Color(0xFF9BA1DA)
+internal val Blue_02 = Color(0xFFBEC3EF)
+internal val Blue_03 = Color(0xFFDDE1FC)
+internal val Blue_04 = Color(0xFFEEF0FF)
 
 // Button Color
-const val activatedButton = 0xFF020514
-const val inactiveButton = 0xFFDDDFEA
+internal val ActivatedColor = Color(0xFF020514)
+internal val InactiveColor = Color(0xFFDDDFEA)
 
 // Typography Color
-const val black = 0xFF000000
-const val gray11 = 0xFF1C1B1F
-const val gray10 = 0xFF313033
-const val gray9 = 0xFF484649
-const val gray8 = 0xFF605D62
-const val gray7 = 0xFF787579
-const val gray6 = 0xFF939094
-const val gray5 = 0xFFAEAAAE
-const val gray4 = 0xFFCECECE
-const val gray3 = 0xFFE9E9E9
-const val gray2 = 0xFF3553F2
-const val gray1 = 0xFFF4F4F4
-const val white = 0xFFFFFFFF
-
-
-const val searchBarText = 0xFF939094
-const val SecondColor = 0xFFEEF0FF
-const val Gray05 = 0xFFAEAAAE
-const val Gray11 = 0xFF1C1B1F
+internal val Gray_11 = Color(0xFF1C1B1F)
+internal val Gray_10 = Color(0xFF313033)
+internal val Gray_09 = Color(0xFF484649)
+internal val Gray_08 = Color(0xFF605D62)
+internal val Gray_07 = Color(0xFF787579)
+internal val gray_06 = Color(0xFF939094)
+internal val gray_05 = Color(0xFFAEAAAE)
+internal val Gray_04 = Color(0xFFCECECE)
+internal val Gray_03 = Color(0xFFE9E9E9)
+internal val Gray_02 = Color(0xFFF4F4F4)
+internal val Gray_01 = Color(0xFFFAFAFA)
