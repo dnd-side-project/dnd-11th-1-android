c450c19c0b19a83afd98d8992214f95025cbf7c3
uselessNaming <seohojon@naver.com>
2024-08-07 00:23:15 +0900

    Chore: serialization plugin 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,5 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.hilt.android) apply false
     alias(libs.plugins.kotlin.kapt) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
 }
