57ba0ad97789129477a88c47aafb9741ad427972
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-08 22:13:16 +0900

    style: 네비 관련 로직만 남겨두고 수정

--- app/src/main/java/com/materip/matetrip/navigation/NavHost.kt
+++ app/src/main/java/com/materip/matetrip/navigation/NavHost.kt
@@ -1,17 +1,15 @@
 package com.materip.matetrip.navigation
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
+import com.materip.matetrip.component.BackButtonTopAppBar
+import com.materip.matetrip.component.BackButtonWithTitleTopAppBar
 import com.materip.matetrip.component.MateTripTopAppBar
 import com.materip.matetrip.ui.FormScreen
 import com.materip.matetrip.ui.HomeScreen
@@ -23,57 +21,49 @@ import com.materip.matetrip.ui.ReviewScreen
 @Composable
 fun SetUpNavGraph(
     navController: NavHostController,
-    startDestination: String = Screen.SignIn.route
+    modifier: Modifier = Modifier,
+    startDestination: String = Screen.Home.route
 ) {
-    val currentBackStack by navController.currentBackStackEntryAsState()
-    val currentDestination = currentBackStack?.destination
-    val currentRoute = currentDestination?.route ?: Screen.Home.route
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        modifier = modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+    ) {
+        composable(Screen.Home.route) {
+            HomeScreen()
+        }
+        composable(Screen.Post.route) {
+            PostScreen()
+        }
+        composable(Screen.Form.route) {
+            FormScreen()
+        }
+        composable(Screen.Profile.route) {
+            ProfileScreen()
+        }
+        composable(Screen.Notification.route) {
+            NotificationScreen()
+        }
+        composable(Screen.Review.route) {
+            ReviewScreen()
+        }
+    }
+}
 
-    Scaffold(
-        topBar = {
-            MateTripTopAppBar(
-//                currentRoute = currentRoute,
-//                onNavigateUp = { navController.navigateUp() }
+@Composable
+fun GetTopBar(currentRoute: String?, navController: NavHostController) {
+    when (currentRoute) {
+        Screen.Home.route -> { MateTripTopAppBar() }
+        Screen.OnBoarding.route -> {
+            BackButtonTopAppBar(onNavigateUp = { navController.navigateUp() } )
+        }
+        else -> {
+            BackButtonWithTitleTopAppBar(
+                screenTitle = "동행 모집하기",
+                onNavigateUp = { navController.navigateUp() }
             )
-        },
-        bottomBar = { /* BottomBar() */ }
-    ) { innerPadding ->
-        NavHost(
-            navController = navController,
-            startDestination = startDestination,
-            modifier = Modifier
-                .fillMaxSize()
-                .verticalScroll(rememberScrollState())
-                .padding(innerPadding)
-        ) {
-//            composable(Screen.SignIn.route) {
-//                SignInScreen(
-//                    onSignInSuccess = { navController.navigate(Screen.Home.route) },
-//                )
-//            }
-//            composable(Screen.OnBoarding.route) {
-//                OnBoardingScreen(
-//                    onComplete = { navController.navigate(Screen.Home.route) }
-//                )
-//            }
-            composable(Screen.Home.route) {
-                HomeScreen()
-            }
-            composable(Screen.Post.route) {
-                PostScreen()
-            }
-            composable(Screen.Form.route) {
-                FormScreen()
-            }
-            composable(Screen.Profile.route) {
-                ProfileScreen()
-            }
-            composable(Screen.Notification.route) {
-                NotificationScreen()
-            }
-            composable(Screen.Review.route) {
-                ReviewScreen()
-            }
         }
     }
 }
