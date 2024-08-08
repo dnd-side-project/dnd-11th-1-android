71dcbb928ea8dd01da0eea19ffc84356429edf8c
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-08 22:17:53 +0900

    chore: app, design, home 모듈에서 compileOptions, kotlinOptions, composeOptions 통일
    - main브랜치에 merge된 login 모듈 의존성에 맞춰 컴파일, 코틀린, 컴포즈 버전 통일함

--- core-designsystem/build.gradle.kts
+++ core-designsystem/build.gradle.kts
@@ -24,17 +24,17 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1" // 1.5.14는 Kotlin 1.9.24를 타겟팅
+        kotlinCompilerExtensionVersion = "1.5.2"
     }
 }
 
--- feature-home/build.gradle.kts
+++ feature-home/build.gradle.kts
@@ -25,11 +25,17 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.2"
     }
 }
 
@@ -50,6 +56,8 @@ dependencies {
 
     //hilt
     implementation(libs.bundles.hilt.impl)
+    implementation(libs.androidx.material3.android)
+    implementation(libs.androidx.ui.tooling.preview.android)
     kapt(libs.bundles.hilt.kapt)
 
     implementation(libs.androidx.core.ktx)
