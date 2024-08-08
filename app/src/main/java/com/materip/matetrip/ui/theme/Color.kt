7ad6f166e6e06b5e18dec99efc2ae5e3b53c6747
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-09 04:49:13 +0900

    design: Color.kt에서 기본값으로 설정된 컬러 추가

--- app/src/main/java/com/materip/matetrip/ui/theme/Color.kt
+++ app/src/main/java/com/materip/matetrip/ui/theme/Color.kt
@@ -5,6 +5,14 @@ import androidx.compose.ui.graphics.Color
 /**
  * 메이트립 Android Color
  */
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
 object MateTripColors {
     // Main Color
     val Primary = Color(0xFF3553F2)
