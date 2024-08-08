70f07af5c1988e5ae52b97629b92df17e1b7c6cc
uselessNaming <seohojon@naver.com>
2024-08-08 16:30:28 +0900

    Resource: Color 파일 Object로 변경

--- core-designsystem/src/main/java/com/materip/matetrip/theme/Color.kt
+++ core-designsystem/src/main/java/com/materip/matetrip/theme/Color.kt
@@ -5,30 +5,33 @@ import androidx.compose.ui.graphics.Color
 /**
  * 메이트립 Android Color
  */
-// Main Color
-internal val Primary = Color(0xFF3553F2)
-internal val Blue_01 = Color(0xFF9BA1DA)
-internal val Blue_02 = Color(0xFFBEC3EF)
-internal val Blue_03 = Color(0xFFDDE1FC)
-internal val Blue_04 = Color(0xFFEEF0FF)
+object MatetripColor{
 
-// Button Color
-internal val ActivatedColor = Color(0xFF020514)
-internal val InactiveColor = Color(0xFFDDDFEA)
+    // Main Color
+    val Primary = Color(0xFF3553F2)
+    val Blue_01 = Color(0xFF9BA1DA)
+    val Blue_02 = Color(0xFFBEC3EF)
+    val Blue_03 = Color(0xFFDDE1FC)
+    val Blue_04 = Color(0xFFEEF0FF)
 
-// Typography Color
-internal val Gray_11 = Color(0xFF1C1B1F)
-internal val Gray_10 = Color(0xFF313033)
-internal val Gray_09 = Color(0xFF484649)
-internal val Gray_08 = Color(0xFF605D62)
-internal val Gray_07 = Color(0xFF787579)
-internal val gray_06 = Color(0xFF939094)
-internal val gray_05 = Color(0xFFAEAAAE)
-internal val Gray_04 = Color(0xFFCECECE)
-internal val Gray_03 = Color(0xFFE9E9E9)
-internal val Gray_02 = Color(0xFFF4F4F4)
-internal val Gray_01 = Color(0xFFFAFAFA)
+    // Button Color
+    val ActivatedColor = Color(0xFF020514)
+    val InactiveColor = Color(0xFFDDDFEA)
 
-//kakao button
-internal val KakaoButtonColor = Color(0xFFF6DB4F)
-internal val KakaoTextColor = Color(0xFF382929)
+    // Typography Color
+    val Gray_11 = Color(0xFF1C1B1F)
+    val Gray_10 = Color(0xFF313033)
+    val Gray_09 = Color(0xFF484649)
+    val Gray_08 = Color(0xFF605D62)
+    val Gray_07 = Color(0xFF787579)
+    val gray_06 = Color(0xFF939094)
+    val gray_05 = Color(0xFFAEAAAE)
+    val Gray_04 = Color(0xFFCECECE)
+    val Gray_03 = Color(0xFFE9E9E9)
+    val Gray_02 = Color(0xFFF4F4F4)
+    val Gray_01 = Color(0xFFFAFAFA)
+
+    //kakao button
+    val KakaoButtonColor = Color(0xFFF6DB4F)
+    val KakaoTextColor = Color(0xFF382929)
+}
