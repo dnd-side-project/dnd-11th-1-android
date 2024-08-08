ec11809f4b429603c580f461762ced0bf228296c
uselessNaming <seohojon@naver.com>
2024-08-08 20:19:48 +0900

    Feat: icon 크기 조절 및 색상 변경 추가

--- core-designsystem/src/main/java/com/materip/matetrip/component/Card.kt
+++ core-designsystem/src/main/java/com/materip/matetrip/component/Card.kt
@@ -9,8 +9,11 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
@@ -20,6 +23,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.materip.matetrip.icon.Icons.man_icon
 import com.materip.matetrip.theme.MateTripTypographySet
@@ -33,11 +37,14 @@ fun OnboardingElevatedCard(
     modifier: Modifier = Modifier,
     text: String,
     icon: Int,
+    tint: Color? = null,
+    iconSize: Dp? = null,
     isSelected: Boolean, // 선택된 카드인지 여부, 이후는 부모 컴포저블에서 관리
     onClick: () -> Unit // 선택된 카드의 상태관리는 부모 컴포저블에서 관리
 ) {
     val backgroundColor = if (isSelected) Color(0xFF3553F2) else Color(0xFFEEF0FF)
     val textColor = if (isSelected) Color.White else MateTripTypographySet.onboardingCard.color
+    val iconColor = if(tint == null) LocalContentColor.current else tint
 
     Card(
         colors = CardDefaults.cardColors(containerColor = backgroundColor),
@@ -58,11 +65,15 @@ fun OnboardingElevatedCard(
                 modifier = Modifier.fillMaxWidth().padding(top = 20.dp, end = 16.dp, bottom = 16.dp),
                 horizontalArrangement = Arrangement.End
             ){
-                Image(
+                Icon(
+                    modifier = if(iconSize != null){
+                        Modifier.size(iconSize)
+                    } else {
+                        Modifier
+                    },
                     painter = painterResource(id = icon),
                     contentDescription = "$text 아이콘",
-                    modifier = Modifier
-                        .padding(start = 65.dp, top = 20.dp)
+                    tint = iconColor
                 )
             }
         }
@@ -91,6 +102,7 @@ fun OnboardingCardSelectionPreview() {
             modifier = Modifier.weight(1f).aspectRatio(1f),
             text = "남성",
             icon = man_icon,
+            iconSize = 30.dp,
             isSelected = selectedCards.contains("남성"),
             onClick = { toggleSelection("남성") }
         )
