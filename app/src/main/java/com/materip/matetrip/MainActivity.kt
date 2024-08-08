0c340a141f68a7380c65cc62e8161255d09f460e
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-08 22:13:49 +0900

    style: Scaffold를 메인에 위치

--- app/src/main/java/com/materip/matetrip/MainActivity.kt
+++ app/src/main/java/com/materip/matetrip/MainActivity.kt
@@ -4,7 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.materip.matetrip.component.MateTripBottomBar
+import com.materip.matetrip.navigation.GetTopBar
+import com.materip.matetrip.navigation.Screen
 import com.materip.matetrip.navigation.SetUpNavGraph
 import com.materip.matetrip.ui.theme.MatetripTheme
 
@@ -14,8 +22,30 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MatetripTheme {
-                val navHost = rememberNavController()
-                SetUpNavGraph(navHost)
+                val navController = rememberNavController()
+                val currentBackStack by navController.currentBackStackEntryAsState()
+                val currentRoute = currentBackStack?.destination?.route
+
+                Scaffold(
+                    topBar = {
+                        GetTopBar(
+                            currentRoute = currentRoute,
+                            navController = navController
+                        )
+                    },
+                    bottomBar = {
+                        MateTripBottomBar(
+                            onHomeClick = { navController.navigate(Screen.Home.route) },
+                            onMyPageClick = { navController.navigate(Screen.MyPage.route) },
+                            onSettingClick = { navController.navigate(Screen.Setting.route) }
+                        )
+                    }
+                ) { innerPadding ->
+                    SetUpNavGraph(
+                        navController = navController,
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
             }
         }
     }
