cb6ccabaf4fed7027be425ce69ed1131ee1b783f
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-08 22:12:35 +0900

    feat: 마이페이지, 설정 경로 추가

--- app/src/main/java/com/materip/matetrip/navigation/Screen.kt
+++ app/src/main/java/com/materip/matetrip/navigation/Screen.kt
@@ -6,7 +6,7 @@ import com.materip.matetrip.icon.Logo
 import com.materip.matetrip.icon.Icons
 
 sealed class Screen(val route: String, @StringRes val resourceId: Int, val icon: Int? = null) {
-    data object SignIn : Screen("signIn", R.string.sign_in)
+    data object SignIn : Screen("sign_in", R.string.sign_in)
     data object OnBoarding : Screen("onBoarding", R.string.onboarding, Icons.arrow_back_icon)
     data object Home : Screen("home", R.string.home, Logo.splash_icon_02)
     data object Post : Screen("post", R.string.post, Icons.arrow_back_icon)
@@ -14,6 +14,8 @@ sealed class Screen(val route: String, @StringRes val resourceId: Int, val icon:
     data object Profile : Screen("profile", R.string.profile, Icons.arrow_back_icon)
     data object Notification : Screen("notification", R.string.notification, Icons.arrow_back_icon)
     data object Review : Screen("review", R.string.review, Icons.arrow_back_icon)
+    data object MyPage : Screen("myPage", R.string.my_page, Icons.arrow_back_icon)
+    data object Setting : Screen("setting", R.string.setting, Icons.arrow_back_icon)
 
     companion object {
         fun fromRoute(route: String?): Screen {
@@ -26,6 +28,8 @@ sealed class Screen(val route: String, @StringRes val resourceId: Int, val icon:
                 Profile.route -> Profile
                 Notification.route -> Notification
                 Review.route -> Review
+                MyPage.route -> MyPage
+                Setting.route -> Setting
                 else -> Home
             }
         }
