ffe3996f68ca22d58293231de60747ca51bb6729
uselessNaming <seohojon@naver.com>
2024-08-09 16:44:17 +0900

    Feat: error state invalid 여부 확인 함수 추가

--- core-common/src/main/java/com/materip/core_common/ErrorState.kt
+++ core-common/src/main/java/com/materip/core_common/ErrorState.kt
@@ -3,11 +3,19 @@ package com.materip.core_common
 sealed interface ErrorState {
     data class NoAuthError(
         val generalError: Pair<Boolean, String?>
-    ): ErrorState
+    ): ErrorState {
+        fun inInvalid(): Boolean{
+            return generalError.first
+        }
+    }
     data class AuthError(
         val invalidTokenError: Boolean,
         val notFoundTokenError: Boolean,
         val generalError: Pair<Boolean, String?>
-    ): ErrorState
+    ): ErrorState {
+        fun isInvalid(): Boolean{
+            return invalidTokenError || notFoundTokenError || generalError.first
+        }
+    }
     data object Loading: ErrorState
 }
