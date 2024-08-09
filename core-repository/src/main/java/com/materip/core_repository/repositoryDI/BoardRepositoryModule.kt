47732cceadfc14edad90946d3a3170c87d7705be
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-10 01:30:25 +0900

    feat: BoardRepositoryImpl을 BoardRepository에 바인딩하는 Hilt 모듈 추가
    - Singleton으로 BoardRepository를 제공하는 BoardRepositoryModule
    - BoardRepositoryModule을 통해 BoardRepositoryImpl을 BoardRepository에 바인딩
    - BoardRepository를 싱글톤으로 제공하기 위해 Hilt 모듈을 추가

---
+++ core-repository/src/main/java/com/materip/core_repository/repositoryDI/BoardRepositoryModule.kt
@@ -0,0 +1,17 @@
+package com.materip.core_repository.repositoryDI
+
+import com.materip.core_repository.repository.BoardRepository
+import com.materip.core_repository.repository.BoardRepositoryImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface BoardRepositoryModule {
+    @Binds
+    @Singleton
+    fun bindBoardRepositoryImpl(boardRepository: BoardRepositoryImpl): BoardRepository
+}
