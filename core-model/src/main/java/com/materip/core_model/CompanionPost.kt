e36be74d7823b5cc2042996025642d2481e1a6da
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-09 09:21:34 +0900

    feat: HomeScreen, 동행라운지를 위한 데이터 클래스 선언

---
+++ core-model/src/main/java/com/materip/core_model/CompanionPost.kt
@@ -0,0 +1,9 @@
+package com.materip.core_model
+
+data class CompanionPost(
+    val image: String,
+    val tags: List<String>,
+    val username: String,
+    val title: String,
+    val duration: String
+)
