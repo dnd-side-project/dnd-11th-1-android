d39d5ef77293714bb3c326dbec76bdbb0b7120a9
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-10 01:31:40 +0900

    feat: 게시글 정보를 담는 BoardRequestDto 데이터 클래스 정의
    - 게시글 작성 요청에 필요한 데이터를 담는 DTO(Data Transfer Object)를 추가
    - 게시글 생성 API 요청에 사용될 BoardRequestDto 클래스

---
+++ core-model/src/main/java/com/materip/core_model/BoardRequestDto.kt
@@ -0,0 +1,27 @@
+package com.materip.core_model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class BoardRequestDto(
+    val title: String,
+    val content: String,
+    val region: String,
+    val startDate: String,
+    val endDate: String,
+    val capacity: Int,
+    val category: String,
+    val preferredAge: String,
+    val preferredGender: String,
+    val imageUrls: List<String>,
+    val tagNames: List<String>
+)
+
+// 임시로 만든 데이터 클래스
+data class CompanionPost(
+    val image: String,
+    val tags: List<String>,
+    val username: String,
+    val title: String,
+    val duration: String
+)
