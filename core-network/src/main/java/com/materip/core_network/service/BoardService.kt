96858e46e6aabb16fa0f0cc7f419d3c1e44b5738
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-10 01:36:52 +0900

    feat: Retrofit을 사용하여 동행글 API 통신을 위한 BoardService 인터페이스 선언
    - 게시글 관련 API 호출을 담당하는 Retrofit 서비스 인터페이스를 정의
    - 페이징 처리를 위한 page, size 쿼리 파라미터를 포함하는 getBoard 메서드 추가
    - @GET, @POST 어노테이션을 사용하여 API 엔드포인트를 지정한 BoardService 인터페이스 추가

---
+++ core-network/src/main/java/com/materip/core_network/service/BoardService.kt
@@ -0,0 +1,20 @@
+package com.materip.core_network.service
+
+import com.materip.core_model.BoardRequestDto
+import com.materip.core_model.BoardResponseDto
+import com.skydoves.sandwich.ApiResponse
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+interface BoardService {
+    @GET("/api/v1/accompany/boards")
+    suspend fun getBoard(
+        @Query("page") page: Int = 0,
+        @Query("size") size: Int = 0
+    ): ApiResponse<BoardResponseDto>
+
+    @POST("/api/v1/accompany/boards")
+    suspend fun postBoard(@Body board: BoardRequestDto): ApiResponse<BoardRequestDto>
+}
