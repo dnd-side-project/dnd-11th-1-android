12383ead4ba9825a33e5faed3974698be3c9d703
uselessNaming <seohojon@naver.com>
2024-08-09 17:49:05 +0900

    Feat: response dto 변경

--- core-network/src/main/java/com/materip/core_network/service/onboarding/OnboardingService.kt
+++ core-network/src/main/java/com/materip/core_network/service/onboarding/OnboardingService.kt
@@ -1,19 +1,16 @@
 package com.materip.core_network.service.onboarding
 
 import com.materip.core_model.request.SaveOnboardingRequestDto
-import com.materip.core_model.response.DefaultResponseDto
-import com.materip.core_model.response.IsOnboardingCompletedResponseDto
+import com.materip.core_model.response.SaveOnboardingResponseDto
 import com.skydoves.sandwich.ApiResponse
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface OnboardingService {
-    /** 임의 response dto */
     @POST("/api/v1/profiles")
-    suspend fun saveOnboardingResult(@Body requestDto: SaveOnboardingRequestDto): ApiResponse<DefaultResponseDto>
+    suspend fun saveOnboardingResult(@Body requestDto: SaveOnboardingRequestDto): ApiResponse<SaveOnboardingResponseDto>
 
-    /** 임의 response dto */
     @GET("/api/v1/profiles/exist")
-    suspend fun isOnboardingCompleted(): ApiResponse<IsOnboardingCompletedResponseDto>
+    suspend fun isOnboardingCompleted(): ApiResponse<Boolean>
 }
