4eda6c5a02223a14ab739fd3804b273d906059d6
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-10 01:39:59 +0900

    chore: file 삭제

--- core-model/src/main/java/com/materip/core_model/CompanionPost.kt
+++
@@ -1,9 +0,0 @@
-package com.materip.core_model
-
-data class CompanionPost(
-    val image: String,
-    val tags: List<String>,
-    val username: String,
-    val title: String,
-    val duration: String
-)
--- feature-home3/src/main/java/com/materip/feature_home/ui/PostScreen.kt
+++
@@ -1,8 +0,0 @@
-package com.materip.feature_home.ui
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun PostScreen() {
-    // PostScreen implementation
-}
