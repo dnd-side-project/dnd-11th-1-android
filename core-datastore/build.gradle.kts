476daaf5b3518f7e99b30e40ded16f538838debb
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-10 01:37:42 +0900

    chore: core-repository 의존성을 core-datastore에 추가

--- core-datastore/build.gradle.kts
+++ core-datastore/build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
     implementation(project(":core-model"))
     implementation(project(":core-network"))
     implementation(project(":core-common"))
+    implementation(project(":core-repository"))
 
     //hilt
     implementation(libs.bundles.hilt.impl)
