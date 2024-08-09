6a647243338513a5595eba304a1033840de2edd6
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-10 01:25:31 +0900

    feat: BoardRepository 인터페이스 추가
    - UI 계층과 데이터 계층 사이의 데이터 흐름을 관리하는 BoardRepository 인터페이스 정의
    - 게시글 관련 데이터 처리를 담당하는 BoardRepository 인터페이스
    - 게시글 데이터 가져오기 및 게시글 작성 기능을 위한 getBoard, postBoard 메서드를 포함

---
+++ core-repository/src/main/java/com/materip/core_repository/repository/BoardRepository.kt
@@ -0,0 +1,12 @@
+package com.materip.core_repository.repository
+// repository모듈은 데이터 소스와의 상호작용을 추상화함
+
+import com.materip.core_common.ResultResponse
+import com.materip.core_model.BoardRequestDto
+import com.materip.core_model.BoardResponseDto
+
+// 게시글 생성 메서드를 정의
+interface BoardRepository {
+    suspend fun getBoard(): ResultResponse<BoardResponseDto>
+    suspend fun postBoard(board: BoardRequestDto): ResultResponse<BoardRequestDto>
+}
