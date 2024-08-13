f1a4d8b5997a39b658b4aa14c245a60dd2454a58
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-13 20:00:54 +0900

    feat: HomeIntent 이미지 업데이트 기능 추가

--- feature-home3/src/main/java/com/materip/feature_home/intent/HomeIntent.kt
+++ feature-home3/src/main/java/com/materip/feature_home/intent/HomeIntent.kt
@@ -3,12 +3,13 @@ package com.materip.feature_home.intent
 sealed class HomeIntent {
     data class UpdateTitle(val title: String) : HomeIntent()
     data class UpdateContent(val content: String) : HomeIntent()
-    data class UpdateTags(val tags: List<String>) : HomeIntent()
+    data class UpdateTagNames(val tagNames: List<String>) : HomeIntent()
     data class UpdateRegion(val region: String) : HomeIntent()
     data class UpdateStartDate(val startDate: String) : HomeIntent()
     data class UpdateEndDate(val endDate: String) : HomeIntent()
-    data class UpdateType(val type: String) : HomeIntent()
-    data class UpdateAge(val age: String) : HomeIntent()
-    data class UpdateGender(val gender: String) : HomeIntent()
+    data class UpdateCategory(val category: String) : HomeIntent()
+    data class UpdateAge(val preferredAge: String) : HomeIntent()
+    data class UpdateGender(val preferredGender: String) : HomeIntent()
     data class UpdateCapacity(val capacity: Int) : HomeIntent()
+    data class UpdateImageUris(val imageUris: List<String>) : HomeIntent()
 }
