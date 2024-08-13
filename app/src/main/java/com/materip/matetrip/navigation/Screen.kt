1d3c54f0f95cd49b5f20a5ea0cca90aac5cec288
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-14 01:13:52 +0900

    feat: 홈 게시글 진입 화면 추가

--- app/src/main/java/com/materip/matetrip/navigation/Screen.kt
+++ app/src/main/java/com/materip/matetrip/navigation/Screen.kt
@@ -9,6 +9,7 @@ sealed class Screen(val route: String, @StringRes val resourceId: Int, val icon:
     data object SignIn : Screen("sign_in", R.string.sign_in)
     data object OnBoarding : Screen("onBoarding", R.string.onboarding, Icons.arrow_back_icon)
     data object Home : Screen("home", R.string.home, Logo.splash_icon_02)
+    data object NavigateToPost : Screen("navigateToPost", R.string.navigate_to_post, Icons.arrow_back_icon)
     data object Post : Screen("post", R.string.post, Icons.arrow_back_icon)
     data object Form : Screen("form", R.string.form, Icons.arrow_back_icon)
     data object Profile : Screen("profile", R.string.profile, Icons.arrow_back_icon)
@@ -23,6 +24,7 @@ sealed class Screen(val route: String, @StringRes val resourceId: Int, val icon:
                 SignIn.route -> SignIn
                 OnBoarding.route -> OnBoarding
                 Home.route -> Home
+                NavigateToPost.route -> NavigateToPost
                 Post.route -> Post
                 Form.route -> Form
                 Profile.route -> Profile
