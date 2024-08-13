02134abf52aca1ede4c9ea0a43b5178b846a7fe5
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-14 01:31:06 +0900

    feat: BoardDataStore에 게시글 상세 조회 메서드 추가

--- core-datastore/src/main/java/com/materip/core_datastore/BoardDataStore.kt
+++ core-datastore/src/main/java/com/materip/core_datastore/BoardDataStore.kt
@@ -1,10 +1,12 @@
 package com.materip.core_datastore
 
 import com.materip.core_common.ResultResponse
-import com.materip.core_model.BoardRequestDto
-import com.materip.core_model.BoardResponseDto
+import com.materip.core_model.accompany_board.BoardRequestDto
+import com.materip.core_model.accompany_board.BoardResponseDto
+import com.materip.core_model.accompany_board.id.GetBoardDetailDto
 
 interface BoardDataStore {
     suspend fun getBoard(): ResultResponse<BoardResponseDto>
     suspend fun postBoard(board: BoardRequestDto): ResultResponse<BoardRequestDto>
+    suspend fun getBoardDetail(id: Int): ResultResponse<GetBoardDetailDto>
 }
