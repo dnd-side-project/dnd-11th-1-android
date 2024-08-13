64c90bb8b0f28786ac85c45c30e2e3a09bf03fb8
uselessNaming <seohojon@naver.com>
2024-08-13 15:49:57 +0900

    Feat: button 클릭 이벤트 추가

--- core-designsystem/src/main/java/com/materip/matetrip/component/View.kt
+++ core-designsystem/src/main/java/com/materip/matetrip/component/View.kt
@@ -2,6 +2,7 @@ package com.materip.matetrip.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
