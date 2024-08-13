5532d650775611fd4f91fbe16d182b0636920bc1
uselessNaming <seohojon@naver.com>
2024-08-13 15:46:36 +0900

    Feat: CustomButton 컴포넌트 추가

--- core-designsystem/src/main/java/com/materip/matetrip/component/Button.kt
+++ core-designsystem/src/main/java/com/materip/matetrip/component/Button.kt
@@ -1,20 +1,33 @@
 package com.materip.matetrip.component
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import com.materip.core_designsystem.R
 import com.materip.matetrip.theme.MateTripTypographySet
 import com.materip.matetrip.theme.MatetripColor.ActivatedColor
 import com.materip.matetrip.theme.MatetripColor.Blue_04
@@ -155,6 +168,47 @@ fun CreateReviewButton(
     }
 }
 
+@Composable
+fun CustomButton(
+    modifier: Modifier = Modifier,
+    trailingIcon: Int? = null,
+    shape: Shape,
+    btnText: String,
+    fontSize: TextUnit,
+    textColor: Color = Color.Black,
+    btnColor: Color,
+    onClick: () -> Unit
+){
+    Button(
+        modifier = modifier,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = btnColor
+        ),
+        shape = shape,
+        onClick = onClick
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ){
+            if(trailingIcon != null){
+                Image(
+                    modifier = Modifier.size(18.dp),
+                    painter = painterResource(trailingIcon),
+                    contentDescription = "Review Icon"
+                )
+                Spacer(Modifier.width(8.dp))
+            }
+            Text(
+                text = btnText,
+                fontSize = fontSize,
+                color = textColor,
+                fontFamily = FontFamily(Font(R.font.noto_sans_kr)),
+                fontWeight = FontWeight(500)
+            )
+        }
+    }
+}
 
 @Preview
 @Composable
