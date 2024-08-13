5160b7bc2ace690218d2dfaec80a55b4740d6a42
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-14 01:29:02 +0900

    BoardRepository에 getBoardDetail 메서드 추가하여 게시글 상세 정보 조회 기능 구현

--- core-repository/src/main/java/com/materip/core_repository/repository/BoardRepository.kt
+++ core-repository/src/main/java/com/materip/core_repository/repository/BoardRepository.kt
@@ -2,11 +2,13 @@ package com.materip.core_repository.repository
 // repository모듈은 데이터 소스와의 상호작용을 추상화함
 
 import com.materip.core_common.ResultResponse
-import com.materip.core_model.BoardRequestDto
-import com.materip.core_model.BoardResponseDto
+import com.materip.core_model.accompany_board.BoardRequestDto
+import com.materip.core_model.accompany_board.BoardResponseDto
+import com.materip.core_model.accompany_board.id.GetBoardDetailDto
 
 // 게시글 생성 메서드를 정의
 interface BoardRepository {
     suspend fun getBoard(): ResultResponse<BoardResponseDto>
     suspend fun postBoard(board: BoardRequestDto): ResultResponse<BoardRequestDto>
+    suspend fun getBoardDetail(id: Int): ResultResponse<GetBoardDetailDto>
 }
