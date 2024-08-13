d8cc65df0dab490b2577f68f2d882174adad14e1
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-14 01:17:21 +0900

    feat: 홈 게시글 진입 화면 추가

---
+++ feature-home3/src/main/java/com/materip/feature_home/ui/NavigateToPostScreen.kt
@@ -0,0 +1,15 @@
+package com.materip.feature_home.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun NavigateToPostScreen() {
+    //
+}
+
+@Preview
+@Composable
+fun NavigateToPostScreenPreview() {
+    NavigateToPostScreen()
+}
