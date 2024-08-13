f4ef3378f9f5a058847912bf7e1df85b3a09edd3
uselessNaming <seohojon@naver.com>
2024-08-14 02:54:20 +0900

    Style: 코드 스타일 변경

--- core-designsystem/src/main/java/com/materip/matetrip/component/View.kt
+++ core-designsystem/src/main/java/com/materip/matetrip/component/View.kt
@@ -1,5 +1,6 @@
 package com.materip.matetrip.component
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -16,20 +17,30 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -66,7 +77,8 @@ fun ImageLoadView(
     onCloseClick: () -> Unit,
 ){
     Box(
-        modifier = Modifier.size(size)
+        modifier = Modifier
+            .size(size)
             .background(color = backgroundColor, shape = shape)
             .clip(shape)
     ){
@@ -82,7 +94,8 @@ fun ImageLoadView(
         )
         if(isEnabledClose){
             Box(
-                modifier = Modifier.fillMaxSize()
+                modifier = Modifier
+                    .fillMaxSize()
                     .padding(top = 6.dp, end = 6.dp),
                 contentAlignment = Alignment.TopEnd
             ){
@@ -115,8 +128,12 @@ fun TravelPostItem(
     Row(
         modifier = modifier
             .height(115.dp)
-            .border(width = 1.dp, color = MatetripColor.Blue_03, shape = RoundedCornerShape(size = 10.dp))
-            .clickable{onClick()}
+            .border(
+                width = 1.dp,
+                color = MatetripColor.Blue_03,
+                shape = RoundedCornerShape(size = 10.dp)
+            )
+            .clickable { onClick() }
             .padding(horizontal = 14.dp, vertical = 15.5.dp)
     ){
         Row(
@@ -126,8 +143,12 @@ fun TravelPostItem(
             Column {
                 Row{
                     Text(
-                        modifier = Modifier.height(27.dp)
-                            .background(color = MatetripColor.Primary, shape = RoundedCornerShape(size = 6.dp))
+                        modifier = Modifier
+                            .height(27.dp)
+                            .background(
+                                color = MatetripColor.Primary,
+                                shape = RoundedCornerShape(size = 6.dp)
+                            )
                             .padding(horizontal = 10.dp, vertical = 5.dp),
                         text = destination,
                         fontSize = 12.sp,
@@ -137,8 +158,12 @@ fun TravelPostItem(
                     )
                     Spacer(Modifier.width(10.dp))
                     Text(
-                        modifier = Modifier.height(27.dp)
-                            .background(color = MatetripColor.Blue_04, shape = RoundedCornerShape(size = 6.dp))
+                        modifier = Modifier
+                            .height(27.dp)
+                            .background(
+                                color = MatetripColor.Blue_04,
+                                shape = RoundedCornerShape(size = 6.dp)
+                            )
                             .padding(horizontal = 10.dp, vertical = 5.dp),
                         text = period,
                         fontSize = 12.sp,
@@ -169,6 +194,204 @@ fun TravelPostItem(
                 contentScale = ContentScale.Crop,
                 model = postImage,
                 contentDescription = "Post Image",
+            )
+        }
+    }
+}
+
+@Composable
+fun QuizItem(
+    indexNumber: Int,
+    title: String,
+    onUpdateTitle: (String) -> Unit,
+    answer: String,
+    onUpdateAnswer: (String) -> Unit,
+){
+    val keyboardController = LocalSoftwareKeyboardController.current
+    var isClicked by remember{mutableStateOf(false)}
+    var titleText by remember{mutableStateOf(title)}
+    var answerText by remember{mutableStateOf(answer)}
+    var readOnly by remember{mutableStateOf(title.isNotEmpty())}
+
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ){
+        Row(
+            modifier = Modifier
+                .height(62.dp)
+                .border(
+                    width = 1.dp,
+                    color = MatetripColor.Blue_03,
+                    shape = if (isClicked) RoundedCornerShape(
+                        topStart = 10.dp,
+                        topEnd = 10.dp
+                    ) else RoundedCornerShape(10.dp)
+                )
+                .clickable { isClicked = !isClicked }
+                .padding(horizontal = 12.dp, vertical = 19.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Box(
+                modifier = Modifier
+                    .size(24.dp)
+                    .background(color = MatetripColor.Blue_03, shape = CircleShape)
+                    .clip(CircleShape),
+                contentAlignment = Alignment.Center
+            ){
+                Text(
+                    text = "${indexNumber}",
+                    fontSize = 14.sp,
+                    fontFamily = FontFamily(Font(R.font.roboto_medium)),
+                    fontWeight = FontWeight(500),
+                    color = MatetripColor.Gray_10
+                )
+            }
+            Spacer(Modifier.width(8.dp))
+            BasicTextField(
+                modifier = Modifier
+                    .weight(1f)
+                    .height(60.dp)
+                    .background(color = Color.White, shape = RoundedCornerShape(10.dp)),
+                readOnly = readOnly,
+                value = titleText,
+                onValueChange = {
+                    titleText = it
+                    onUpdateTitle(it)
+                },
+                singleLine = true,
+                textStyle = TextStyle(
+                    color = MatetripColor.Gray_07,
+                    fontSize = 16.sp,
+                    fontFamily = FontFamily(Font(R.font.noto_sans_kr)),
+                    fontWeight = FontWeight(500)
+                ),
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        keyboardController?.hide()
+                        readOnly = true
+                    }
+                )
+            ){
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ){
+                    if(titleText.isNotEmpty()){
+                        it()
+                    } else {
+                        Text(
+                            text = "제목을 작성해 주세요.",
+                            fontSize = 16.sp,
+                            fontFamily = FontFamily(Font(R.font.noto_sans_kr)),
+                            fontWeight = FontWeight(400),
+                            color = MatetripColor.Gray_07
+                        )
+                    }
+                }
+            }
+            Icon(
+                modifier = Modifier.size(14.dp),
+                painter = painterResource(if(isClicked) Icons.fold_icon else Icons.navigate_next_icon),
+                contentDescription = "Fold Down Icon"
+            )
+        }
+        if(isClicked){
+            BasicTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(60.dp)
+                    .background(
+                        color = MatetripColor.Blue_04,
+                        shape = RoundedCornerShape(bottomEnd = 10.dp, bottomStart = 10.dp)
+                    ),
+                value = answerText,
+                onValueChange = {
+                    answerText = it
+                    onUpdateAnswer(it)
+                },
+                singleLine = true,
+                textStyle = TextStyle(
+                    color = MatetripColor.Gray_07,
+                    fontSize = 14.sp,
+                    fontFamily = FontFamily(Font(R.font.noto_sans_kr)),
+                    fontWeight = FontWeight(400)
+                )
+            ){
+                Row(
+                    modifier = Modifier.padding(start = 20.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ){
+                    Text(
+                        text = " · ",
+                        color = MatetripColor.Gray_07,
+                        fontSize = 14.sp,
+                        fontFamily = FontFamily(Font(R.font.noto_sans_kr)),
+                        fontWeight = FontWeight(400)
+                    )
+                    if(answerText.isNotEmpty()){
+                        it()
+                    } else {
+                        Text(
+                            text = "내용을 입력해주세요",
+                            fontSize = 14.sp,
+                            fontFamily = FontFamily(Font(R.font.noto_sans_kr)),
+                            fontWeight = FontWeight(400),
+                            color = MatetripColor.Gray_07
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun QuizEditItem(
+    indexNumber: Int,
+    title: String,
+    isClicked: Boolean,
+    onClick: () -> Unit,
+){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(62.dp)
+            .border(width = 1.dp, color = MatetripColor.Blue_03, shape = RoundedCornerShape(10.dp))
+            .padding(horizontal = 12.dp, vertical = 19.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Box(
+            modifier = Modifier
+                .size(24.dp)
+                .background(color = MatetripColor.Blue_03, shape = CircleShape),
+            contentAlignment = Alignment.Center
+        ){
+            Text(
+                text = "${indexNumber}",
+                fontSize = 14.sp,
+                fontFamily = FontFamily(Font(R.font.noto_sans_kr)),
+                fontWeight = FontWeight(500),
+                color = MatetripColor.Gray_10
+            )
+        }
+        Spacer(Modifier.width(8.dp))
+        Text(
+            text = title,
+            fontSize = 16.sp,
+            fontFamily = FontFamily(Font(R.font.noto_sans_kr)),
+            fontWeight = FontWeight(500)
+        )
+        Spacer(Modifier.weight(1f))
+        IconButton(
+            modifier = Modifier.size(20.dp),
+            onClick = onClick
+        ) {
+            Image(
+                modifier = Modifier.fillMaxSize(),
+                painter = painterResource(if(isClicked) Icons.selected_radio_btn else Icons.not_selected_radio_btn),
+                contentDescription = "Radio Button"
             )
         }
     }
