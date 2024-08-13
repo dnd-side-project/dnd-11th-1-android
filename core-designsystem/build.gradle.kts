7733ee56eea7b2b4a68e0a6693bf287fc022a5ee
uselessNaming <seohojon@naver.com>
2024-08-13 15:47:03 +0900

    Chore: UI bundle 의존성 추가

--- core-designsystem/build.gradle.kts
+++ core-designsystem/build.gradle.kts
@@ -41,8 +41,8 @@ android {
 dependencies {
     implementation(project(":core-model"))
 
-    //coil
-    implementation(libs.bundles.coil)
+    implementation(libs.bundles.coil) // coil
+    implementation(libs.bundles.ui) // ui
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
