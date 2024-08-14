f523cf65cdbb07ac0959a78d9ba4114ab1446d56
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-15 01:34:54 +0900

    feat: HomeIntent 타입 수정

--- feature-home3/src/main/java/com/materip/feature_home/intent/HomeIntent.kt
+++ feature-home3/src/main/java/com/materip/feature_home/intent/HomeIntent.kt
@@ -7,7 +7,7 @@ sealed class HomeIntent {
     data class UpdateRegion(val region: String) : HomeIntent()
     data class UpdateStartDate(val startDate: String) : HomeIntent()
     data class UpdateEndDate(val endDate: String) : HomeIntent()
-    data class UpdateCategory(val category: String) : HomeIntent()
+    data class UpdateCategory(val category: List<String>) : HomeIntent()
     data class UpdateAge(val preferredAge: String) : HomeIntent()
     data class UpdateGender(val preferredGender: String) : HomeIntent()
     data class UpdateCapacity(val capacity: Int) : HomeIntent()
