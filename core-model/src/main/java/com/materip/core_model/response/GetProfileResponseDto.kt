c323178e58d33754d6d6cc03057844074d2e95c0
uselessNaming <seohojon@naver.com>
2024-08-15 14:57:22 +0900

    Feat: get profile response 모델 추가

---
+++ core-model/src/main/java/com/materip/core_model/response/GetProfileResponseDto.kt
@@ -0,0 +1,16 @@
+package com.materip.core_model.response
+
+data class GetProfileResponseDto(
+    val userId: Int,
+    val nickname: String,
+    val provider: String,
+    val profileImageUrl: String,
+    val description: String,
+    val gender: String,
+    val birthYear: Int,
+    val socialMediaUrl: String,
+    val grade: String,
+    val travelPreferences: List<String>,
+    val travelStyles: List<String>,
+    val foodPreferences: List<String>
+)
