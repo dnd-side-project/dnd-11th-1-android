d73f866528f5e6e452f965135d2247b2d3c2ea38
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-17 02:44:46 +0900

    feat: add postCompanionRequest, update getBoard

--- core-repository/src/main/java/com/materip/core_repository/repository/BoardRepositoryImpl.kt
+++ core-repository/src/main/java/com/materip/core_repository/repository/BoardRepositoryImpl.kt
@@ -2,17 +2,19 @@ package com.materip.core_repository.repository
 
 import com.materip.core_common.ResultResponse
 import com.materip.core_datastore.BoardDataStore
-import com.materip.core_model.accompany_board.BoardRequestDto
-import com.materip.core_model.accompany_board.BoardResponseDto
+import com.materip.core_model.accompany_board.create.BoardRequestDto
+import com.materip.core_model.accompany_board.BoardListResponse
+import com.materip.core_model.accompany_board.Pageable
 import com.materip.core_model.accompany_board.id.BoardIdDto
 import com.materip.core_model.accompany_board.id.GetBoardDetailDto
+import com.materip.core_model.accompany_board.request.CompanionRequest
 import javax.inject.Inject
 
 class BoardRepositoryImpl @Inject constructor(
     private val boardDataStore: BoardDataStore
 ) : BoardRepository {
-    override suspend fun getBoard(): ResultResponse<BoardResponseDto> {
-        return boardDataStore.getBoard()
+    override suspend fun getBoard(pageable: Pageable): ResultResponse<BoardListResponse> {
+        return boardDataStore.getBoard(pageable)
     }
 
     override suspend fun postBoard(board: BoardRequestDto): ResultResponse<BoardIdDto> {
@@ -21,5 +23,9 @@ class BoardRepositoryImpl @Inject constructor(
 
     override suspend fun getBoardDetail(id: Int): ResultResponse<GetBoardDetailDto> {
         return boardDataStore.getBoardDetail(id)
+    }
+
+    override suspend fun postCompanionRequest(companionRequest: CompanionRequest): ResultResponse<Unit> {
+        return boardDataStore.postCompanionRequest(companionRequest)
     }
 }
