5f8d088a27205858e63597ed0cab71e608a18338
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-17 02:42:14 +0900

    feat: add postCompanionRequest, update getBoard

--- core-datastore/src/main/java/com/materip/core_datastore/BoardDataStore.kt
+++ core-datastore/src/main/java/com/materip/core_datastore/BoardDataStore.kt
@@ -1,13 +1,16 @@
 package com.materip.core_datastore
 
 import com.materip.core_common.ResultResponse
-import com.materip.core_model.accompany_board.BoardRequestDto
-import com.materip.core_model.accompany_board.BoardResponseDto
+import com.materip.core_model.accompany_board.create.BoardRequestDto
+import com.materip.core_model.accompany_board.BoardListResponse
+import com.materip.core_model.accompany_board.Pageable
 import com.materip.core_model.accompany_board.id.BoardIdDto
 import com.materip.core_model.accompany_board.id.GetBoardDetailDto
+import com.materip.core_model.accompany_board.request.CompanionRequest
 
 interface BoardDataStore {
-    suspend fun getBoard(): ResultResponse<BoardResponseDto>
+    suspend fun getBoard(pageable: Pageable): ResultResponse<BoardListResponse>
     suspend fun postBoard(board: BoardRequestDto): ResultResponse<BoardIdDto>
     suspend fun getBoardDetail(id: Int): ResultResponse<GetBoardDetailDto>
+    suspend fun postCompanionRequest(companionRequest: CompanionRequest): ResultResponse<Unit>
 }
