c8b29f51b05602bdcc323f3b24587519f4a5ab48
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-18 15:06:47 +0900

    feat: 동행글 상세 조회 데이터 클래스 변수 수정

--- core-model/src/main/java/com/materip/core_model/accompany_board/id/ProfileThumbnail.kt
+++ core-model/src/main/java/com/materip/core_model/accompany_board/id/ProfileThumbnail.kt
@@ -2,15 +2,12 @@ package com.materip.core_model.accompany_board.id
 
 import kotlinx.serialization.Serializable
 
-// 동행글 상세 조회 ProfileInfo
+// 동행글 상세 조회 ProfileThumbnail
 @Serializable
-data class ProfileInfo(
+data class ProfileThumbnail(
+    val userId: Int,
     val nickname: String,
     val profileImageUrl: String,
-    val provider: String,
     val birthYear: Int,
     val gender: String,
-    val travelPreferences: List<String>,
-    val travelStyles: List<String>,
-    val foodPreferences: List<String>
 )
