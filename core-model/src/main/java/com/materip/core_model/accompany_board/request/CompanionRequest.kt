0f6287bec2f02e910b908a5c1258ced2955723c4
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-18 15:13:50 +0900

    feat: 알림 화면을 위한 알림 내역 유무를 판단하는 메서드 추가

--- core-model/src/main/java/com/materip/core_model/accompany_board/request/CompanionRequest.kt
+++ core-model/src/main/java/com/materip/core_model/accompany_board/request/CompanionRequest.kt
@@ -2,9 +2,15 @@ package com.materip.core_model.accompany_board.request
 
 import kotlinx.serialization.Serializable
 
+// TODO: API 동행 신청할 때 보낸 시각도 추가해야 함. 그래야 알림 받을 때 시각 표시
 @Serializable
 data class CompanionRequest(
     val boardId: Int,
     val introduce: String,
     val chatLink: String
-)
+) {
+    // 알림 화면에서 보여줄 내용이 있는지 확인
+    fun hasContent(): Boolean {
+        return introduce.isNotEmpty() && chatLink.isNotEmpty()
+    }
+}
