ade7eb2faa407eb14a2094abe86770f6fe45d3b2
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-18 15:08:20 +0900

    feat: 알림 화면 구현

--- feature-home3/src/main/java/com/materip/feature_home/ui/NotificationScreen.kt
+++ feature-home3/src/main/java/com/materip/feature_home/ui/NotificationScreen.kt
@@ -1,8 +1,126 @@
 package com.materip.feature_home.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.SubcomposeAsyncImage
+import com.materip.core_model.accompany_board.id.ProfileThumbnail
+import com.materip.core_model.accompany_board.notification.Notification
+import com.materip.feature_home.state.NotificationUiState
+import com.materip.feature_home.viewModel.NotificationViewModel
+import com.materip.matetrip.icon.Badges.profile_default_badge
+import com.materip.matetrip.icon.Icons.notification_none_icon
+import com.materip.matetrip.theme.MateTripColors.Blue_04
+import com.materip.matetrip.theme.MateTripColors.Gray_05
+import com.materip.matetrip.theme.MateTripTypographySet
 
 @Composable
-fun NotificationScreen() {
-    // FormScreen implementation
+fun NotificationScreen(
+    viewModel: NotificationViewModel = hiltViewModel()
+) {
+    when (val uiState = viewModel.uiState.collectAsState().value) {
+        is NotificationUiState.Initial ->
+            DefaultNotification()
+
+        is NotificationUiState.Loading -> {
+            CircularProgressIndicator()
+        }
+
+        is NotificationUiState.Success -> {
+            /* TODO: Success 상태에 대한 UI 로직 추가 */
+//            NotificationList(
+//                notifications = uiState.notifications,
+//                profileThumbnail = uiState.profileThumbnail
+//            )
+        }
+
+        is NotificationUiState.Error -> {
+            /* TODO: Error 상태에 대한 UI 로직 추가 */
+        }
+    }
+}
+
+@Composable
+private fun NotificationList(
+    notifications: List<Notification>,
+    profileThumbnail: ProfileThumbnail
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(12.dp, Alignment.Top),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.padding(start = 20.dp)
+    ) {
+        notifications.forEach { notification ->
+            val isSentByUser = profileThumbnail.userId == notification.userId
+
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(10.dp, Alignment.Start),
+                verticalAlignment = Alignment.Top,
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(38.dp)
+                        .background(shape = CircleShape, color = Blue_04)
+                ) {
+                    if (profileThumbnail.profileImageUrl.isEmpty()) {
+                        Image(
+                            painter = painterResource(id = profile_default_badge),
+                            contentDescription = "프로필 기본 이미지",
+                        )
+                    } else {
+                        SubcomposeAsyncImage(
+                            model = profileThumbnail.profileImageUrl,
+                            contentDescription = "Network Image",
+                            contentScale = ContentScale.Crop
+                        )
+                    }
+                }
+                if (isSentByUser) {
+                    Text(text = notification.getFormattedMessage())
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun DefaultNotification() {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(12.dp, Alignment.CenterVertically),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Image(
+            painter = painterResource(id = notification_none_icon),
+            contentDescription = "notification_none_icon",
+            contentScale = ContentScale.None
+        )
+        Text(
+            text = "아직 알림 내역이 없어요.",
+            style = MateTripTypographySet.numberMedium2,
+            color = Gray_05
+        )
+    }
+}
+
+@Preview
+@Composable
+fun NotificationScreenPreview() {
+    NotificationScreen()
 }
