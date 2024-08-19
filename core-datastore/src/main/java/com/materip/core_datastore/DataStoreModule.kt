b894f69573adb143f9e2b5f9fe47d78bba58c264
uselessNaming <seohojon@naver.com>
2024-08-19 11:21:14 +0900

    Feat: image data store DI

--- core-datastore/src/main/java/com/materip/core_datastore/DataStoreModule.kt
+++ core-datastore/src/main/java/com/materip/core_datastore/DataStoreModule.kt
@@ -6,6 +6,8 @@ import com.materip.core_datastore.com.materip.core_datastore.login_datastore.Rem
 import com.materip.core_datastore.com.materip.core_datastore.login_datastore.RemoteLoginDataStoreImpl
 import com.materip.core_datastore.com.materip.core_datastore.onboarding_datastore.OnboardingDataStore
 import com.materip.core_datastore.com.materip.core_datastore.onboarding_datastore.OnboardingDataStoreImpl
+import com.materip.core_datastore.image_datastore.ImageDataStore
+import com.materip.core_datastore.image_datastore.ImageDataStoreImpl
 import com.materip.core_datastore.profile_datastore.ProfileDataStore
 import com.materip.core_datastore.profile_datastore.ProfileDataStoreImpl
 import com.materip.core_datastore.test_datastore.TestDataStore
@@ -38,4 +40,8 @@ interface DataStoreModule {
     @Singleton
     @Binds
     fun provideProfileDataStore(profileDataStoreImpl: ProfileDataStoreImpl): ProfileDataStore
+
+    @Singleton
+    @Binds
+    fun provideImageDataStore(imageDataStoreImpl: ImageDataStoreImpl): ImageDataStore
 }
