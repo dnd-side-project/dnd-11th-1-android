c1b7fb5508445752d9fd52900ff82cd9b6d52730
uselessNaming <seohojon@naver.com>
2024-08-19 17:28:14 +0900

    Feat: setting navigation 경로 모델 추가

---
+++ feature-mypage/src/main/java/com/materip/feature_mypage/navigation/SettingRoute.kt
@@ -0,0 +1,10 @@
+package com.materip.feature_mypage.navigation
+
+enum class SettingRoute {
+    SettingGraph,
+    AccountInfoScreen,
+    AlarmSettingScreen,
+    GetAuthCodeScreen,
+    SMSVerificationScreen,
+    SettingScreen
+}
