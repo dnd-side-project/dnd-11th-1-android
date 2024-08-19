6e4566439f7063663ea2a2f2e9406dc16f55cc26
uselessNaming <seohojon@naver.com>
2024-08-20 00:48:31 +0900

    Test: test용 navigation 정리

--- feature-login/src/main/java/com/materip/feature_login/navigation/LoginNavigation.kt
+++ feature-login/src/main/java/com/materip/feature_login/navigation/LoginNavigation.kt
@@ -4,10 +4,8 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.materip.feature_login.LoginRoute
-import com.materip.feature_login.screens.ScreenTest
 
 fun NavController.navigateToLogin() = navigate(LoginRoute.LoginRoute.name)
-fun NavController.navigateToTest() = navigate("TEST_SCREEN")
 
 fun NavGraphBuilder.login(
     navOnBoarding: () -> Unit,
@@ -18,11 +16,5 @@ fun NavGraphBuilder.login(
             navOnBoarding = navOnBoarding,
             navMyPage = navMyPage
         )
-    }
-}
-
-fun NavGraphBuilder.test(){
-    composable(route = "TEST_SCREEN"){
-        ScreenTest()
     }
 }
