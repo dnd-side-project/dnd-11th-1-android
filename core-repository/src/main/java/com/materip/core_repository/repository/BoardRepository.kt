84f463f59d8f9a907451a934520f6bcc52000f47
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-19 16:47:10 +0900

    chore: 주석 추가

--- core-repository/src/main/java/com/materip/core_repository/repository/BoardRepository.kt
+++ core-repository/src/main/java/com/materip/core_repository/repository/BoardRepository.kt
@@ -11,8 +11,15 @@ import com.materip.core_model.accompany_board.request.CompanionRequest
 
 // 게시글 생성 메서드를 정의
 interface BoardRepository {
+    // 동행글 목록 조회
     suspend fun getBoard(pageable: Pageable): ResultResponse<BoardListResponse>
+    // 동행글 생성
     suspend fun postBoard(board: BoardRequestDto): ResultResponse<BoardIdDto>
+    // 동행글 상세 조회
     suspend fun getBoardDetail(id: Int): ResultResponse<GetBoardDetailDto>
+    // 동행 신청
     suspend fun postCompanionRequest(companionRequest: CompanionRequest): ResultResponse<Unit>
+    // 동행 리뷰
+
+    // 알림 조회
 }
