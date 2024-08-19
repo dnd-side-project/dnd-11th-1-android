73bcc3f269ecaf4647960ed858f35f93dca3e1e8
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-19 17:11:15 +0900

    feat: 알림 화면 UI, 비즈니스 로직 연동

--- feature-home3/src/main/java/com/materip/feature_home/ui/NotificationScreen.kt
+++ feature-home3/src/main/java/com/materip/feature_home/ui/NotificationScreen.kt
@@ -1,36 +1,25 @@
 package com.materip.feature_home.ui
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import coil.compose.SubcomposeAsyncImage
-import com.materip.core_model.accompany_board.id.ProfileThumbnail
-import com.materip.core_model.accompany_board.notification.Notification
 import com.materip.feature_home.state.NotificationUiState
 import com.materip.feature_home.viewModel.NotificationViewModel
-import com.materip.matetrip.icon.Badges.profile_default_badge
 import com.materip.matetrip.icon.Icons.notification_none_icon
-import com.materip.matetrip.theme.MateTripColors.Blue_04
 import com.materip.matetrip.theme.MateTripColors.Gray_05
 import com.materip.matetrip.theme.MateTripTypographySet
 
+// TODO: 로그인 정보가 필요해서 병합 후 작업 예정
 @Composable
 fun NotificationScreen(
     viewModel: NotificationViewModel = hiltViewModel()
@@ -45,10 +34,6 @@ fun NotificationScreen(
 
         is NotificationUiState.Success -> {
             /* TODO: Success 상태에 대한 UI 로직 추가 */
-//            NotificationList(
-//                notifications = uiState.notifications,
-//                profileThumbnail = uiState.profileThumbnail
-//            )
         }
 
         is NotificationUiState.Error -> {
@@ -57,48 +42,51 @@ fun NotificationScreen(
     }
 }
 
-@Composable
-private fun NotificationList(
-    notifications: List<Notification>,
-    profileThumbnail: ProfileThumbnail
-) {
-    Column(
-        verticalArrangement = Arrangement.spacedBy(12.dp, Alignment.Top),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.padding(start = 20.dp)
-    ) {
-        notifications.forEach { notification ->
-            val isSentByUser = profileThumbnail.userId == notification.userId
-
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(10.dp, Alignment.Start),
-                verticalAlignment = Alignment.Top,
-            ) {
-                Box(
-                    modifier = Modifier
-                        .size(38.dp)
-                        .background(shape = CircleShape, color = Blue_04)
-                ) {
-                    if (profileThumbnail.profileImageUrl.isEmpty()) {
-                        Image(
-                            painter = painterResource(id = profile_default_badge),
-                            contentDescription = "프로필 기본 이미지",
-                        )
-                    } else {
-                        SubcomposeAsyncImage(
-                            model = profileThumbnail.profileImageUrl,
-                            contentDescription = "Network Image",
-                            contentScale = ContentScale.Crop
-                        )
-                    }
-                }
-                if (isSentByUser) {
-                    Text(text = notification.getFormattedMessage())
-                }
-            }
-        }
-    }
-}
+//@Composable
+//private fun NotificationList(
+//    notifications: List<Notification>,
+//) {
+//    Column(
+//        verticalArrangement = Arrangement.spacedBy(12.dp, Alignment.Top),
+//        horizontalAlignment = Alignment.CenterHorizontally,
+//        modifier = Modifier.padding(start = 20.dp)
+//    ) {
+//        notifications.forEach { notification ->
+//            Row(
+//                horizontalArrangement = Arrangement.spacedBy(10.dp, Alignment.Start),
+//                verticalAlignment = Alignment.Top,
+//            ) {
+//                Box(
+//                    modifier = Modifier
+//                        .size(38.dp)
+//                        .background(shape = CircleShape, color = Blue_04)
+//                ) {
+//                    if (유저프로필조회.profileImageUrl.isEmpty()) {
+//                        Image(
+//                            painter = painterResource(id = profile_default_badge),
+//                            contentDescription = "프로필 기본 이미지",
+//                        )
+//                    } else {
+//                        SubcomposeAsyncImage(
+//                            model = 유저프로필조회.profileImageUrl,
+//                            contentDescription = "Network Image",
+//                            contentScale = ContentScale.Crop
+//                        )
+//                    }
+//                }
+//                val request = notification.boardRequest != null
+//                val review = notification.boardReview != null
+//
+//                if (request) {
+//                    Text(text = "${유저프로필조회.nickname}으로 부터 동행 신청서가 도착했어요.")
+//                }
+//                if (review) {
+//                    Text(text = "${유저프로필조회.nickname}님이 동행글에 리뷰를 남겼어요.")
+//                }
+//            }
+//        }
+//    }
+//}
 
 @Composable
 private fun DefaultNotification() {
