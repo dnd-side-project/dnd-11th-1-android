0404a116551c741797a3b690ce70c5c4f0151c56
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-20 11:00:22 +0900

    feat: package route 변경

--- feature-login/src/main/java/com/materip/feature_login/navigation/LoginNavigation.kt
+++ feature-login/src/main/java/com/materip/feature_login/navigation/LoginNavigation.kt
@@ -1,10 +1,9 @@
 package com.materip.feature_login.navigation
 
-import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import com.materip.feature_login.LoginRoute
+import com.materip.feature_login.screens.LoginRoute
 import com.materip.feature_login.screens.ScreenTest
 
 fun NavController.navigateToLogin() = navigate(LoginRoute.LoginRoute.name)
