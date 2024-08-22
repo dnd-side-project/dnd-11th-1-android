7a4aa1658dc57a5d0f5542a16f89ed3363a76e17
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-22 19:28:29 +0900

    feat: 동행후기 화면 init

---
+++ feature-home3/src/main/java/com/materip/feature_home3/ui/profile_content/ReviewContent.kt
@@ -0,0 +1,10 @@
+package com.materip.feature_home3.ui.profile_content
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ReviewContent() {
+    // 동행후기 화면 구현
+    Text(text = "동행후기")
+}
