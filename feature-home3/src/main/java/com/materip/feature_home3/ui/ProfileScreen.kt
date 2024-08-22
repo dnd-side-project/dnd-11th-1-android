dd3ef9e082aca5bfc11c6a13d063c5a2dc1489ff
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-22 19:38:29 +0900

    feat: ProfileScreen 구현

--- feature-home3/src/main/java/com/materip/feature_home3/ui/ProfileScreen.kt
+++ feature-home3/src/main/java/com/materip/feature_home3/ui/ProfileScreen.kt
@@ -1,8 +1,66 @@
 package com.materip.feature_home3.ui
 
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.materip.feature_home3.state.ProfileUiState
+import com.materip.feature_home3.ui.component.TabRowComponent
+import com.materip.feature_home3.ui.profile_content.ProfileContent
+import com.materip.feature_home3.ui.profile_content.QnaContent
+import com.materip.feature_home3.ui.profile_content.ReviewContent
+import com.materip.feature_home3.viewModel.ProfileViewModel
+
 
 @Composable
-fun ProfileScreen() {
-    // FormScreen implementation
+fun ProfileScreen(
+    boardId: Int,
+    viewModel: ProfileViewModel = hiltViewModel(),
+) {
+    val uiState by viewModel.uiState.collectAsState()
+
+    LaunchedEffect(boardId) {
+        viewModel.getProfileDetails()
+    }
+
+    when (uiState) {
+        is ProfileUiState.Loading -> {
+            CircularProgressIndicator()
+        }
+
+        is ProfileUiState.Success -> {
+            ProfileContent()
+        }
+
+        is ProfileUiState.Error -> {
+            Text("오류: ${(uiState as ProfileUiState.Error).message}")
+        }
+
+        ProfileUiState.Initial -> {
+            // Handle initial state if needed
+        }
+    }
+
+    val tabs = listOf("프로필", "동행후기", "백문백답")
+    val contentScreens = listOf<@Composable () -> Unit>(
+        { ProfileContent() },
+        { ReviewContent() },
+        { QnaContent() }
+    )
+
+    TabRowComponent(
+        tabs = tabs,
+        contentScreens = contentScreens
+    )
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun ProfileScreenPreview() {
+    ProfileScreen(boardId = 1)
 }
