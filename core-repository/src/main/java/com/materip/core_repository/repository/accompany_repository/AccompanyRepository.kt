0f298b9810e5cf988696763492b35e7bc572799f
uselessNaming <seohojon@naver.com>
2024-08-22 15:04:40 +0900

    Feat: accompany repository 추가

---
+++ core-repository/src/main/java/com/materip/core_repository/repository/accompany_repository/AccompanyRepository.kt
@@ -0,0 +1,12 @@
+package com.materip.core_repository.repository.accompany_repository
+
+import com.materip.core_common.ResultResponse
+import com.materip.core_model.accompany_board.Pageable
+import com.materip.core_model.request.AccompanyApplicationResponseDto
+import com.materip.core_model.response.DefaultGetAccompanyResponseDto
+
+interface AccompanyRepository {
+    suspend fun getAccompanyApplication(id: Int, applicantId: Int?): ResultResponse<AccompanyApplicationResponseDto>
+    suspend fun getAccompanySend(pageable: Pageable): ResultResponse<DefaultGetAccompanyResponseDto>
+    suspend fun getAccompanyReceived(pageable: Pageable): ResultResponse<DefaultGetAccompanyResponseDto>
+}
