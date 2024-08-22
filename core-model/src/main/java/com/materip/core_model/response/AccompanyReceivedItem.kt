50c5ee0d710a58c152384d90a4a511ffc37208da
uselessNaming <seohojon@naver.com>
2024-08-22 21:31:35 +0900

    Feat: 파라미터 타입 변경

---
+++ core-model/src/main/java/com/materip/core_model/response/AccompanyReceivedItem.kt
@@ -0,0 +1,37 @@
+package com.materip.core_model.response
+
+import kotlinx.serialization.Serializable
+import java.time.LocalDate
+
+@Serializable
+data class AccompanyReceivedItem(
+    val requestId: Int,
+    val userId: Int,
+    val nickname: String,
+    val provider: String,
+    val profileImageUrl: String?,
+    val description: String?,
+    val gender: String,
+    val birthYear: Int,
+    val socialMediaUrl: String,
+    val grade: String,
+    val travelPreferences: List<String>,
+    val travelStyle: List<String>,
+    val foodPreferences: List<String>,
+    val userImageUrl: List<String>
+){
+    fun getAgeText(): String{
+        val age = LocalDate.now().year - birthYear
+        val temp = when(age%10){
+            in 0..4 -> "초반"
+            in 5..6 -> "중반"
+            else -> "후반"
+        }
+        val ageText = "${age/10}대 ${temp}"
+        return ageText
+    }
+
+    fun getGender(): String{
+        return if (gender == "MALE") "남성" else "여성"
+    }
+}
