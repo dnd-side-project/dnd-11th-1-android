ef2b8d04db6e799fc40682eaf9a51cd1070abb09
uselessNaming <seohojon@naver.com>
2024-08-22 21:31:38 +0900

    Feat: 파라미터 타입 변경

--- core-model/src/main/java/com/materip/core_model/response/GetProfileDetailsResponseDto.kt
+++ core-model/src/main/java/com/materip/core_model/response/GetProfileDetailsResponseDto.kt
@@ -1,6 +1,7 @@
 package com.materip.core_model.response
 
 import kotlinx.serialization.Serializable
+import java.time.LocalDate
 
 @Serializable
 data class GetProfileDetailsResponseDto(
@@ -17,4 +18,23 @@ data class GetProfileDetailsResponseDto(
     val travelStyles: List<String>,
     val foodPreferences: List<String>,
     val userImageUrls: List<String>
-)
+){
+    fun getAgeText(): String{
+        val age = LocalDate.now().year - birthYear
+        val temp = when(age%10){
+            in 0..4 -> "초반"
+            in 5..6 -> "중반"
+            else -> "후반"
+        }
+        val ageText = "${(age/10)}0대 ${temp}"
+        return ageText
+    }
+
+    fun getGenderText(): String{
+        return if(gender == "MALE") "남성" else "여성"
+    }
+
+    fun getTags(): List<String>{
+        return travelStyles.plus(travelPreferences).plus(foodPreferences)
+    }
+}
