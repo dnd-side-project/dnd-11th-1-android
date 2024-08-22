b4ee9765deea3681cacc54a74f30d2781d34f1b6
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-22 20:39:46 +0900

    feat: 매개변수 순서 수정, getAge 함수 추가

--- core-model/src/main/java/com/materip/core_model/response/GetProfileDetailsResponseDto.kt
+++ core-model/src/main/java/com/materip/core_model/response/GetProfileDetailsResponseDto.kt
@@ -1,17 +1,35 @@
 package com.materip.core_model.response
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class GetProfileDetailsResponseDto(
+    val userId: Int,
+    val nickname: String,
+    val provider: String,
+    val gender: String,
     val birthYear: Int,
     val description: String?,
-    val foodPreferences: List<String>,
-    val gender: String,
     val grade: String?,
-    val nickname: String,
     val profileImageUrl: String?,
-    val provider: String,
     val socialMediaUrl: String?,
     val travelPreferences: List<String>,
     val travelStyles: List<String>,
-    val userId: Int,
-    val userImageUrls: List<String>
-)
+    val userImageUrls: List<String>,
+    val foodPreferences: List<String>
+) {
+    fun getAge(): String{
+        return when(birthYear){
+            in 0..9 -> "${birthYear}세"
+            in 10..19 -> "10대"
+            in 20..29 -> "20대"
+            in 30..39 -> "30대"
+            in 40..49 -> "40대"
+            in 50..59 -> "50대"
+            in 60..69 -> "60대"
+            in 70..79 -> "70대"
+            in 80..89 -> "80대"
+            else -> "90대"
+        }
+    }
+}
