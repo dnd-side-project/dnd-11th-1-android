2587e3618a7ea70e82849f1be166f8cf986f930f
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-23 16:17:42 +0900

    feat: ReviewRepository 인터페이스를 구현하는 클래스
    - ReviewRepositoryImpl: ReviewRepository 인터페이스를 구현하는 클래스
    - @Inject constructor: 의존성 주입을 위한 생성자
    - reviewDataStore: ReviewDataStore 타입의 의존성

---
+++ core-repository/src/main/java/com/materip/core_repository/repository/review_repository/ReviewRepositoryImpl.kt
@@ -0,0 +1,10 @@
+package com.materip.core_repository.repository.review_repository
+
+import com.materip.core_datastore.review_datastore.ReviewDataStore
+import javax.inject.Inject
+
+// TODO: 동행후기 ReviewRepositoryImpl
+class ReviewRepositoryImpl @Inject constructor(
+    private val reviewDataStore: ReviewDataStore
+) : ReviewRepository {
+}
