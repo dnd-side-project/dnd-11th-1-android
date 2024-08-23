6f3393733ab53c4d8b6a7dc9a7de285eebdfcdd7
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-23 16:26:54 +0900

    feat: ReviewUiState가 Initial일 때의 ReviewContentInitial 구현

--- feature-home3/src/main/java/com/materip/feature_home3/ui/profile_content/ReviewContent.kt
+++ feature-home3/src/main/java/com/materip/feature_home3/ui/profile_content/ReviewContent.kt
@@ -1,10 +1,114 @@
 package com.materip.feature_home3.ui.profile_content
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.materip.core_designsystem.icon.Icons
+import com.materip.core_designsystem.theme.MateTripColors.Gray_04
+import com.materip.core_designsystem.theme.MateTripTypographySet
+import com.materip.feature_home3.state.ReviewUiState
+import com.materip.feature_home3.viewModel.ReviewViewModel
 
 @Composable
-fun ReviewContent() {
-    // 동행후기 화면 구현
-    Text(text = "동행후기")
+fun ReviewContent(
+    viewModel: ReviewViewModel = hiltViewModel()
+) {
+    val uiState by viewModel.uiState.collectAsState()
+
+    when (uiState) {
+        is ReviewUiState.Loading -> {
+            CircularProgressIndicator()
+        }
+
+        is ReviewUiState.Success -> {
+            // 동행 후기 api 완료되면 그때 다른 UI로 변경
+            ReviewContentInitial()
+        }
+
+        is ReviewUiState.Error -> {
+            Text("오류: ${(uiState as ReviewUiState.Error).message}")
+        }
+
+        ReviewUiState.Initial -> {
+            ReviewContentInitial()
+        }
+    }
+}
+
+
+// ReviewContent의 상태가 Initial일 때의 UI (후기가 없을 때와 동일)
+@Composable
+fun ReviewContentInitial() {
+    Column(
+        modifier = Modifier.padding(start = 20.dp, end = 20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start,
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(180.dp, Alignment.Start),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(text = "받은 동행 평가", style = MateTripTypographySet.headline05)
+                IconButton(
+                    onClick = { /*TODO*/ }
+                ) {
+                    Icon(
+                        painter = painterResource(id = Icons.enter_24_icon),
+                        contentDescription = "받은 동행 평가 더보기"
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(240.dp))
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(180.dp, Alignment.Start),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(text = "받은 동행 후기", style = MateTripTypographySet.headline05)
+                IconButton(
+                    onClick = { /*TODO*/ }
+                ) {
+                    Icon(
+                        painter = painterResource(id = Icons.enter_24_icon),
+                        contentDescription = "받은 동행 후기 더보기"
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(170.dp))
+        }
+        Text(
+            text = "아직 받은 동행 평가와 후기가 없어요.",
+            style = MateTripTypographySet.body03,
+            color = Gray_04,
+            modifier = Modifier.height(24.dp)
+        )
+        Spacer(modifier = Modifier.height(170.dp))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ReviewContentPreview() {
+    ReviewContentInitial()
 }
