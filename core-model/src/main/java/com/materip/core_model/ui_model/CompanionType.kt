de9ff02520ffa1501a59035a6b99cba8ea4dcc3d
uselessNaming <seohojon@naver.com>
2024-08-26 14:40:28 +0900

    Feat: review 데이터 태그 모델 추가

---
+++ core-model/src/main/java/com/materip/core_model/ui_model/CompanionType.kt
@@ -0,0 +1,8 @@
+package com.materip.core_model.ui_model
+
+enum class CompanionType {
+    ALL_ACCOMPANYING,
+    PARTIAL_COMPANIONSHIP,
+    ACCOMMODATION_SHARING,
+    ACCOMPANYING_TOUR
+}
---
+++ core-model/src/main/java/com/materip/core_model/ui_model/TravelPreferenceForReview.kt
@@ -0,0 +1,10 @@
+package com.materip.core_model.ui_model
+
+enum class TravelPreferenceForReview {
+    PLANNED,
+    SPONTANEOUS,
+    PUBLIC_MONEY_CONVENIENT,
+    DUTCH_PAY,
+    DILIGENT,
+    RELAXED
+}
