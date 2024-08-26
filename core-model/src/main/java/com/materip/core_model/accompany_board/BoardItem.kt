0345f7b6769fe8202fd2f92fa27b00d9735ed7f9
uselessNaming <seohojon@naver.com>
2024-08-27 01:12:59 +0900

    Feat: 지역 모델 추가

--- core-model/src/main/java/com/materip/core_model/accompany_board/BoardItem.kt
+++ core-model/src/main/java/com/materip/core_model/accompany_board/BoardItem.kt
@@ -1,5 +1,6 @@
 package com.materip.core_model.accompany_board
 
+import com.materip.core_model.ui_model.Region
 import kotlinx.serialization.Serializable
 import java.time.LocalDate
 import java.time.temporal.ChronoUnit
@@ -26,5 +27,18 @@ data class BoardItem(
         val end = LocalDate.parse(endDate.substring(0,10))
         val duration = ChronoUnit.DAYS.between(start, end)
         return "${duration}박 ${duration + 1}일"
+    }
+    fun getRegionText(): String{
+        return when(region){
+            Region.SEOUL.name -> "수도권"
+            Region.GYEONGGI_INCHEON.name -> "경기·인천"
+            Region.GANGWON.name -> "강원도"
+            Region.CHUNGCHEONG_DAEJON_SEJONG.name -> "충청도"
+            Region.GYEONGSANG_DAEGU_ULSAN.name -> "경상도"
+            Region.BUSAN.name -> "부산"
+            Region.JEOLLA_GWANGJU.name -> "전라도"
+            Region.JEJU.name -> "제주도"
+            else -> "태그 오류"
+        }
     }
 }
---
+++ core-model/src/main/java/com/materip/core_model/ui_model/Region.kt
@@ -0,0 +1,12 @@
+package com.materip.core_model.ui_model
+
+enum class Region {
+    SEOUL,
+    GYEONGGI_INCHEON,
+    CHUNGCHEONG_DAEJON_SEJONG,
+    GANGWON,
+    JEOLLA_GWANGJU,
+    GYEONGSANG_DAEGU_ULSAN,
+    BUSAN,
+    JEJU
+}
