55b4f5dd782c715b0ec7338043302b9ff1b363ea
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-27 12:16:39 +0900

    feat: Home, PostBoard의 Intent 분리

---
+++ feature-home3/src/main/java/com/materip/feature_home3/intent/PostBoardIntent.kt
@@ -0,0 +1,25 @@
+package com.materip.feature_home3.intent
+
+import com.materip.core_model.accompany_board.create.BoardRequestDto
+import com.materip.core_model.accompany_board.create.BoardStatus
+import com.materip.core_model.accompany_board.create.Category
+import com.materip.core_model.accompany_board.create.PreferredAge
+import com.materip.core_model.accompany_board.create.PreferredGender
+import com.materip.core_model.accompany_board.create.Region
+
+sealed class PostBoardIntent {
+    data class UpdateTitle(val title: String) : PostBoardIntent()
+    data class UpdateContent(val content: String) : PostBoardIntent()
+    data class UpdateTagNames(val tagNames: List<String>) : PostBoardIntent()
+    data class UpdateRegion(val region: Region) : PostBoardIntent()
+    data class UpdateStartDate(val startDate: String) : PostBoardIntent()
+    data class UpdateEndDate(val endDate: String) : PostBoardIntent()
+    data class UpdateCategory(val category: List<Category>) : PostBoardIntent()
+    data class UpdateAge(val preferredAge: PreferredAge) : PostBoardIntent()
+    data class UpdateGender(val preferredGender: PreferredGender) : PostBoardIntent()
+    data class UpdateCapacity(val capacity: Int) : PostBoardIntent()
+    data class UpdateBoardStatus(val boardStatus: BoardStatus) : PostBoardIntent()
+    data class UpdateImageUris(val imageUris: List<String>) : PostBoardIntent()
+
+    data class CreatePost(val boardRequestDto: BoardRequestDto) : PostBoardIntent()
+}
