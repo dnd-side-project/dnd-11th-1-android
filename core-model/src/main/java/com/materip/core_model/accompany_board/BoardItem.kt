a5fcad7a9b0a2cec008824794250af7319bf77a2
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-28 23:45:06 +0900

    feat: region 데이터 타입 변경

--- core-model/src/main/java/com/materip/core_model/accompany_board/BoardItem.kt
+++ core-model/src/main/java/com/materip/core_model/accompany_board/BoardItem.kt
@@ -1,5 +1,6 @@
 package com.materip.core_model.accompany_board
 
+import com.materip.core_model.accompany_board.create.Region
 import kotlinx.serialization.Serializable
 
 // 동행글 목록 조회 Data
@@ -7,7 +8,7 @@ import kotlinx.serialization.Serializable
 data class BoardItem(
     val boardId: Int,
     val title: String,
-    val region: String,
+    val region: Region,
     val startDate: String,
     val endDate: String,
     val nickname: String,
