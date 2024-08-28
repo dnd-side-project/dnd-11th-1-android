9a7fb5c8bf963b1716ca51fe2dc7018669195ce9
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-29 04:04:17 +0900

    feat: DeleteBoard 클래스 추가

--- feature-home3/src/main/java/com/materip/feature_home3/intent/HomeIntent.kt
+++ feature-home3/src/main/java/com/materip/feature_home3/intent/HomeIntent.kt
@@ -2,4 +2,5 @@ package com.materip.feature_home3.intent
 
 sealed class HomeIntent {
     data class LoadBoardDetail(val boardId: Int) : HomeIntent()
+    data class DeleteBoard(val boardId: Int) : HomeIntent()
 }
