7ee8d398b33ca235518453ce1dd1a7f71cd4e52f
상은 <71699054+vmkmym@users.noreply.github.com>
2024-08-28 20:12:55 +0900

    feat: HomeUiState 수정

--- feature-home3/src/main/java/com/materip/feature_home3/state/HomeUiState.kt
+++ feature-home3/src/main/java/com/materip/feature_home3/state/HomeUiState.kt
@@ -5,7 +5,6 @@ import com.materip.core_model.accompany_board.id.GetBoardDetailDto
 sealed class HomeUiState {
     data object Initial : HomeUiState()
     data object Loading : HomeUiState()
-    data object SuccessPost : HomeUiState()
     data class SuccessLoad(val boardDetail: GetBoardDetailDto) : HomeUiState()
     data class Error(val message: String) : HomeUiState()
 }
