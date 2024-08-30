2b2b2ace7804cb144c1e24559da32eb9a993886e
uselessNaming <seohojon@naver.com>
2024-08-31 02:45:23 +0900

    Feat: Review Service 함수 추가

--- core-network/src/main/java/com/materip/core_network/service/review/ReviewService.kt
+++ core-network/src/main/java/com/materip/core_network/service/review/ReviewService.kt
@@ -1,13 +1,27 @@
 package com.materip.core_network.service.review
 
 import com.materip.core_model.request.PostReviewRequestDto
+import com.materip.core_model.response.DefaultListResponseDto
+import com.materip.core_model.response.GetReviewDescriptionResponseDto
+import com.materip.core_model.response.GetReviewEvaluationsResponseDto
+import com.materip.core_model.response.ReviewItem
 import com.skydoves.sandwich.ApiResponse
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface ReviewService {
+    @GET("/api/v1/reviews")
+    suspend fun getReviews(): ApiResponse<DefaultListResponseDto<ReviewItem>>
     @POST("/api/v1/reviews")
     suspend fun postReview(
         @Body requestDto: PostReviewRequestDto
     ): ApiResponse<Any>
+    @GET("/api/v1/reviews/{id}")
+    suspend fun getReviewDescription(
+        @Path("id")id: Int
+    ): ApiResponse<GetReviewDescriptionResponseDto>
+    @GET("/api/v1/reviews/evaluations")
+    suspend fun getReviewEvaluations(): ApiResponse<GetReviewEvaluationsResponseDto>
 }
