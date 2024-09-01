8e877982ede5975c4b2fe332821308cdcc49c934
uselessNaming <seohojon@naver.com>
2024-09-01 23:32:57 +0900

    Feat: 파라미터 변수 변경

--- feature-mypage/src/main/java/com/materip/feature_mypage/pager/AccompanyRecordPagingSource.kt
+++ feature-mypage/src/main/java/com/materip/feature_mypage/pager/AccompanyRecordPagingSource.kt
@@ -4,34 +4,32 @@ import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.materip.core_model.accompany_board.BoardItem
 import com.materip.core_model.accompany_board.Pageable
+import com.materip.core_model.request.PagingRequestDto
+import com.materip.core_model.response.BoardItemWithReviewId
 import com.materip.core_repository.repository.accompany_repository.AccompanyRepository
 
 class AccompanyRecordPagingSource(
     val accompanyRepository: AccompanyRepository
-): PagingSource<Int, BoardItem>() {
+): PagingSource<Int, BoardItemWithReviewId>() {
     private var total = 0
-    private var cursor = 0
+    private var cursor: String? = null
 
-    override suspend fun load(params: PagingSource.LoadParams<Int>): PagingSource.LoadResult<Int, BoardItem> {
+    override suspend fun load(params: PagingSource.LoadParams<Int>): LoadResult<Int, BoardItemWithReviewId> {
         val pageNumber = params.key ?: 0
         try{
             val response =
                 accompanyRepository.getAccompanyRecords(
-                    Pageable(
-                        page = cursor,
-                        size = 10,
-                        sort = emptyList()
-                    )
+                    PagingRequestDto(cursor = cursor)
                 )
             if(response.error != null){
                 return LoadResult.Error(Exception(response.error!!.message))
             }
             val result = response.data!!
             total = result.data.size
-            cursor = result.data.last().boardId
+            cursor = result.cursor
             val prevKey = if(pageNumber > 0) pageNumber - 1 else null
             val nextKey = if(result.hasNext) pageNumber + 1 else null
-            return PagingSource.LoadResult.Page(
+            return LoadResult.Page(
                 data = result.data,
                 prevKey = prevKey,
                 nextKey = nextKey
@@ -41,7 +39,7 @@ class AccompanyRecordPagingSource(
         }
     }
 
-    override fun getRefreshKey(state: PagingState<Int, BoardItem>): Int? {
+    override fun getRefreshKey(state: PagingState<Int, BoardItemWithReviewId>): Int? {
         return state.anchorPosition?.let{
             state.closestPageToPosition(it)?.prevKey?.plus(1)
                 ?: state.closestPageToPosition(it)?.nextKey?.minus(1)
--- feature-mypage/src/main/java/com/materip/feature_mypage/pager/ReceiveApplicationPagingSource.kt
+++ feature-mypage/src/main/java/com/materip/feature_mypage/pager/ReceiveApplicationPagingSource.kt
@@ -3,6 +3,7 @@ package com.materip.feature_mypage.pager
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.materip.core_model.accompany_board.Pageable
+import com.materip.core_model.request.PagingRequestDto
 import com.materip.core_model.response.AccompanyReceivedItem
 import com.materip.core_repository.repository.accompany_repository.AccompanyRepository
 
@@ -11,24 +12,20 @@ class ReceiveApplicationPagingSource (
 ): PagingSource<Int, AccompanyReceivedItem>() {
 
     private var total = 0
-    private var cursor = 0
+    private var cursor: String? = null
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, AccompanyReceivedItem> {
         val pageNumber = params.key ?: 0
         try{
             val response = accompanyRepository.getAccompanyReceived(
-                    Pageable(
-                        page = cursor,
-                        size = 10,
-                        sort = emptyList()
-                    )
+                    PagingRequestDto(cursor)
                 )
             if(response.error != null){
                 return LoadResult.Error(Exception(response.error!!.message))
             }
             val result = response.data!!
             total = result.data.size
-            cursor = result.data.last().requestId
+            cursor = result.cursor
             val prevKey = if(pageNumber > 0) pageNumber - 1 else null
             val nextKey = if(result.hasNext) null else pageNumber + 1
             return LoadResult.Page(
