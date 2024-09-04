fa5c6c0db8eeaa055804f80b13b096e8aebc4c6c
상은 <71699054+vmkmym@users.noreply.github.com>
2024-09-04 23:58:21 +0900

    feat: BoardListIntent에 매개변수 수정

--- feature-home3/src/main/java/com/materip/feature_home3/intent/BoardListIntent.kt
+++ feature-home3/src/main/java/com/materip/feature_home3/intent/BoardListIntent.kt
@@ -1,7 +1,7 @@
 package com.materip.feature_home3.intent
 
-import com.materip.core_model.accompany_board.Pageable
+import com.materip.core_model.request.PagingRequestDto
 
 sealed class BoardListIntent {
-    data class LoadBoardList(val pageable: Pageable) : BoardListIntent()
+    data class LoadBoardList(val pagingRequestDto: PagingRequestDto) : BoardListIntent()
 }
