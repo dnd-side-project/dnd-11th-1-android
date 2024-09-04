145716873779c540d7178944591bd372e3ead5ed
상은 <71699054+vmkmym@users.noreply.github.com>
2024-09-04 18:27:37 +0900

    comment: 주석 수정

--- core-model/src/main/java/com/materip/core_model/accompany_board/id/GetBoardDetailDto.kt
+++ core-model/src/main/java/com/materip/core_model/accompany_board/id/GetBoardDetailDto.kt
@@ -2,7 +2,7 @@ package com.materip.core_model.accompany_board.id
 
 import kotlinx.serialization.Serializable
 
-// 동행글 상세 조회 GetBoardDetailDto
+// 동행글 상세 조회 /api/v1/accompany/boards/{id}
 @Serializable
 data class GetBoardDetailDto(
     val boardInfo: BoardInfo,
