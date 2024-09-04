c25ec4a0dc6b2e44bac326de84bc208c57737c27
상은 <71699054+vmkmym@users.noreply.github.com>
2024-09-04 19:08:08 +0900

    refactor: 매개변수 변경

--- core-datastore/src/main/java/com/materip/core_datastore/home_datastore/BoardDataStoreImpl.kt
+++ core-datastore/src/main/java/com/materip/core_datastore/home_datastore/BoardDataStoreImpl.kt
@@ -2,13 +2,13 @@ package com.materip.core_datastore.home_datastore
 
 import com.materip.core_common.ResponseError
 import com.materip.core_common.ResultResponse
+import com.materip.core_model.accompany_board.all.BoardListResponse
 import com.materip.core_model.accompany_board.create.BoardRequestDto
-import com.materip.core_model.accompany_board.BoardListResponse
-import com.materip.core_model.accompany_board.Pageable
 import com.materip.core_model.accompany_board.id.BoardIdDto
 import com.materip.core_model.accompany_board.id.GetBoardDetailDto
 import com.materip.core_model.accompany_board.profile.GetUserProfile
 import com.materip.core_model.accompany_board.request.CompanionRequest
+import com.materip.core_model.request.PagingRequestDto
 import com.materip.core_network.service.home.BoardService
 import com.skydoves.sandwich.message
 import com.skydoves.sandwich.suspendOnError
@@ -20,7 +20,7 @@ import javax.inject.Inject
 class BoardDataStoreImpl @Inject constructor(
     private val boardService: BoardService
 ) : BoardDataStore {
-    override suspend fun getBoard(pageable: Pageable): ResultResponse<BoardListResponse> {
+    override suspend fun getBoard(boardRequest: PagingRequestDto): ResultResponse<BoardListResponse> {
         val result = ResultResponse<BoardListResponse>()
         boardService.getBoard().suspendOnSuccess {
             result.data = this.data
