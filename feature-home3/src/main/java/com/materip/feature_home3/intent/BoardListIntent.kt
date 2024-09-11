5a7c54b57b3b4930e896f0224417556094b7f170
상은 <71699054+vmkmym@users.noreply.github.com>
2024-09-11 20:54:45 +0700

    feat: SearchBoardList intent 추가

--- feature-home3/src/main/java/com/materip/feature_home3/intent/BoardListIntent.kt
+++ feature-home3/src/main/java/com/materip/feature_home3/intent/BoardListIntent.kt
@@ -4,4 +4,5 @@ import com.materip.core_model.request.PagingRequestDto
 
 sealed class BoardListIntent {
     data class LoadBoardList(val pagingRequestDto: PagingRequestDto) : BoardListIntent()
+    data class SearchBoardList(val query: String) : BoardListIntent()
 }
