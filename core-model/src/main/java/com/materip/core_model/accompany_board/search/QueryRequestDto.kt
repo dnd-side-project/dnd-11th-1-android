c65e935be3400d44e98ae383069faaafb4ebbced
상은 <71699054+vmkmym@users.noreply.github.com>
2024-09-11 20:50:03 +0700

    feat: 동행글 검색 데이터 클래스 생성

---
+++ core-model/src/main/java/com/materip/core_model/accompany_board/search/QueryRequestDto.kt
@@ -0,0 +1,10 @@
+package com.materip.core_model.accompany_board.search
+
+import kotlinx.serialization.Serializable
+
+// 동행글 검색 /api/v1/accompany/boards/search
+@Serializable
+data class QueryRequestDto(
+    val cursor: String?,
+    val size: Int = 0
+)
