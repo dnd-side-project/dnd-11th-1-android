e0041c0e226f3af060fb75d44cdd4f3e029aee43
uselessNaming <seohojon@naver.com>
2024-09-11 22:12:12 +0900

    Rename: route 변경

---
+++ core-model/src/main/java/com/materip/core_model/navigation/MyPageRoute.kt
@@ -0,0 +1,15 @@
+package com.materip.core_model.navigation
+
+enum class MyPageRoute {
+    MyPageGraph,
+    MyPageRoute,
+    EditProfileRoute,
+    ProfileDescriptionRoute,
+    Quiz100Route,
+    PreviewRoute,
+    ReviewEvaluationRoute,
+    ReviewListRoute,
+    ReviewDescriptionRoute,
+    SendApplicationRoute,
+    WriteReviewRoute
+}
---
+++ core-model/src/main/java/com/materip/core_model/navigation/SettingRoute.kt
@@ -0,0 +1,14 @@
+package com.materip.core_model.navigation
+
+enum class SettingRoute {
+    SettingGraph,
+    AccountInfoRoute,
+    AlarmSettingRoute,
+    GetAuthCodeRoute,
+    SMSVerificationRoute,
+    SettingRoute,
+    AccountDeletionNoticeRoute,
+    DeleteAccountDoneRoute,
+    DeleteAccountRoute,
+    LogoutRoute,
+}
