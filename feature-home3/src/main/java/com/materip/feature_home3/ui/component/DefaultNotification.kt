309c3e5f068db1994cbc6c4ddb3886369daa81fb
상은 <71699054+vmkmym@users.noreply.github.com>
2024-09-17 03:24:12 +0900

    design: DefaultNotification UI 수정

--- feature-home3/src/main/java/com/materip/feature_home3/ui/component/DefaultNotification.kt
+++ feature-home3/src/main/java/com/materip/feature_home3/ui/component/DefaultNotification.kt
@@ -3,9 +3,11 @@ package com.materip.feature_home3.ui.component
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -16,13 +18,14 @@ import com.materip.core_designsystem.theme.MateTripTypographySet
 @Composable
 fun DefaultNotification() {
     Column(
+        modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.spacedBy(12.dp, Alignment.CenterVertically),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Image(
             painter = painterResource(id = notification_none_icon),
             contentDescription = "notification_none_icon",
-            contentScale = ContentScale.None
+            contentScale = ContentScale.None,
         )
         Text(
             text = "아직 알림 내역이 없어요.",
