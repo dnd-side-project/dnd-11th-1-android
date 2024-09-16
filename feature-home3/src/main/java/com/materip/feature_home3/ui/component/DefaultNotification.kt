418f86aae1aece158c5ae03a53ef9124109e7e13
상은 <71699054+vmkmym@users.noreply.github.com>
2024-09-17 03:18:48 +0900

    rename: DefaultNotification 메서드 이동

---
+++ feature-home3/src/main/java/com/materip/feature_home3/ui/component/DefaultNotification.kt
@@ -0,0 +1,33 @@
+package com.materip.feature_home3.ui.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.materip.core_designsystem.icon.Icons.notification_none_icon
+import com.materip.core_designsystem.theme.MateTripColors.Gray_05
+import com.materip.core_designsystem.theme.MateTripTypographySet
+
+@Composable
+fun DefaultNotification() {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(12.dp, Alignment.CenterVertically),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Image(
+            painter = painterResource(id = notification_none_icon),
+            contentDescription = "notification_none_icon",
+            contentScale = ContentScale.None
+        )
+        Text(
+            text = "아직 알림 내역이 없어요.",
+            style = MateTripTypographySet.numberMedium2,
+            color = Gray_05
+        )
+    }
+}
