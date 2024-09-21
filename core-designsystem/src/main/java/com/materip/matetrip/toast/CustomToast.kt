739e1169c6257adc40d4ece13e3f497b3c74f391
uselessNaming <seohojon@naver.com>
2024-09-21 20:51:37 +0900

    Feat: error 상태 custom toast 추가

---
+++ core-designsystem/src/main/java/com/materip/matetrip/toast/CustomToast.kt
@@ -0,0 +1,39 @@
+package com.materip.matetrip.toast
+
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.platform.LocalSavedStateRegistryOwner
+import androidx.lifecycle.setViewTreeLifecycleOwner
+import androidx.lifecycle.setViewTreeViewModelStoreOwner
+import androidx.savedstate.setViewTreeSavedStateRegistryOwner
+
+class CustomToast(context: Context): Toast(context) {
+    @Composable
+    fun MakeText(
+        message: String,
+        icon: Int,
+        duration: Int = LENGTH_SHORT
+    ){
+        val context = LocalContext.current
+        val views = ComposeView(context)
+
+        views.setContent{
+            CustomToastUtil.SetView(
+                message = message,
+                resourceIcon = icon
+            )
+        }
+
+        views.setViewTreeLifecycleOwner(LocalLifecycleOwner.current)
+
+        views.setViewTreeSavedStateRegistryOwner(LocalSavedStateRegistryOwner.current)
+
+        this.duration = duration
+        this.view = views
+        this.show()
+    }
+}
---
+++ core-designsystem/src/main/java/com/materip/matetrip/toast/ErrorView.kt
@@ -0,0 +1,36 @@
+package com.materip.matetrip.toast
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalContext
+import com.materip.core_common.ErrorState
+import com.materip.core_designsystem.icon.Badges
+import kotlinx.coroutines.delay
+
+@Composable
+fun ErrorView(
+    errState: ErrorState,
+    navBack: () -> Unit,
+){
+    val message = when(errState){
+        is ErrorState.AuthError -> {
+            if (errState.invalidTokenError){
+                "인증되지 않은 사용자입니다."
+            } else if (errState.notFoundTokenError){
+                "존재하지 않는 사용자입니다."
+            } else {
+                errState.generalError.second!!
+            }
+        }
+        is ErrorState.NoAuthError -> errState.generalError.second!!
+        else -> "오류가 아닙니다"
+    }
+
+    val customToast = CustomToast(LocalContext.current)
+    customToast.MakeText(message = message, icon = Badges.warning_badge)
+
+    LaunchedEffect(Unit){
+        delay(2_000)
+        navBack()
+    }
+}
