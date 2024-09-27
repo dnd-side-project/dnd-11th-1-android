5f3d9ddd270093247310fa2b99e73d3a7cf2918b
상은 <71699054+vmkmym@users.noreply.github.com>
2024-09-27 22:13:22 +0900

    refactor: 서버에서 받는 데이터 타입으로 수정

--- core-model/src/main/java/com/materip/core_model/accompany_board/create/BoardFormState.kt
+++ core-model/src/main/java/com/materip/core_model/accompany_board/create/BoardFormState.kt
@@ -5,6 +5,7 @@ import com.materip.core_model.ui_model.Category
 import com.materip.core_model.ui_model.PreferredAge
 import com.materip.core_model.ui_model.PreferredGender
 import com.materip.core_model.ui_model.Region
+import java.time.LocalDateTime
 
 data class BoardFormState(
     val title: String = "",
@@ -14,8 +15,8 @@ data class BoardFormState(
     val tagNames: List<String> = emptyList(),
     val category: List<Category> = emptyList(),
     val region: Region? = null,
-    val startDate: String = "",
-    val endDate: String = "",
+    val startDate: LocalDateTime ?= null,
+    val endDate: LocalDateTime ?= null,
     val capacity: Int = 2,
     val imageUris: List<String> = emptyList(),
     val boardStatus: BoardStatus = BoardStatus.RECRUITING
