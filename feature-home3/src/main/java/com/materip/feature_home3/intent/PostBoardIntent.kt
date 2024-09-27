87086a02ceae24b993f52be03137489fa5b54576
상은 <71699054+vmkmym@users.noreply.github.com>
2024-09-27 22:12:20 +0900

    refactor: 서버에서 받는 데이터 타입으로 수정

--- feature-home3/src/main/java/com/materip/feature_home3/intent/PostBoardIntent.kt
+++ feature-home3/src/main/java/com/materip/feature_home3/intent/PostBoardIntent.kt
@@ -7,14 +7,15 @@ import com.materip.core_model.ui_model.Category
 import com.materip.core_model.ui_model.PreferredAge
 import com.materip.core_model.ui_model.PreferredGender
 import com.materip.core_model.ui_model.Region
+import java.time.LocalDateTime
 
 sealed class PostBoardIntent {
     data class UpdateTitle(val title: String) : PostBoardIntent()
     data class UpdateContent(val content: String) : PostBoardIntent()
     data class UpdateTagNames(val tagNames: List<String>) : PostBoardIntent()
     data class UpdateRegion(val region: Region) : PostBoardIntent()
-    data class UpdateStartDate(val startDate: String) : PostBoardIntent()
-    data class UpdateEndDate(val endDate: String) : PostBoardIntent()
+    data class UpdateStartDate(val startDate: LocalDateTime) : PostBoardIntent()
+    data class UpdateEndDate(val endDate: LocalDateTime) : PostBoardIntent()
     data class UpdateCategory(val category: List<Category>) : PostBoardIntent()
     data class UpdateAge(val preferredAge: PreferredAge) : PostBoardIntent()
     data class UpdateGender(val preferredGender: PreferredGender) : PostBoardIntent()
