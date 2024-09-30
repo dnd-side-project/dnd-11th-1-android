b26fed460f48cea9777ef77e337618476256be60
상은 <71699054+vmkmym@users.noreply.github.com>
2024-09-30 19:51:07 +0900

    rename: ShowRecruitment 파일 이동
    - 컴포넌트 폴더로 파일 이동
    
    Fixes: #36

---
+++ feature-home3/src/main/java/com/materip/feature_home3/ui/component/ShowRecruitment.kt
@@ -0,0 +1,54 @@
+package com.materip.feature_home3.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.materip.core_designsystem.theme.MateTripColors.Blue_04
+import com.materip.core_designsystem.theme.MateTripTypographySet
+
+@Composable
+fun ShowRecruitment(headCount: Int, capacity: Int) {
+    Column(
+        modifier = Modifier.padding(start = 20.dp, end = 20.dp, top = 50.dp, bottom = 40.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(10.dp, Alignment.Top),
+            horizontalAlignment = Alignment.Start,
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(240.dp, Alignment.CenterHorizontally),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(text = "모집 인원", style = MateTripTypographySet.headline05)
+                Box(
+                    modifier = Modifier
+                        .width(41.dp)
+                        .height(24.dp)
+                        .background(color = Blue_04, shape = RoundedCornerShape(size = 54.dp))
+                        .padding(start = 10.dp, top = 4.dp, end = 10.dp, bottom = 4.dp),
+                ) {
+                    Text(
+                        text = "$headCount/$capacity",
+                        style = MateTripTypographySet.numberRegular3,
+                        color = Color(0xFF555555),
+                        fontWeight = FontWeight(500)
+                    )
+                }
+            }
+        }
+    }
+}
