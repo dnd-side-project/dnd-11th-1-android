43bf8b2b639e79be39c6e42751757e6c71153b22
상은 <71699054+vmkmym@users.noreply.github.com>
2024-10-02 20:17:07 +0900

    fix: SuccessDelete 매개변수 제거
    Fixes #41

--- feature-home3/src/main/java/com/materip/feature_home3/state/HomeUiState.kt
+++ feature-home3/src/main/java/com/materip/feature_home3/state/HomeUiState.kt
@@ -1,12 +1,11 @@
 package com.materip.feature_home3.state
 
-import com.materip.core_model.accompany_board.id.BoardIdDto
 import com.materip.core_model.accompany_board.id.GetBoardDetailDto
 
 sealed class HomeUiState {
     data object Initial : HomeUiState()
     data object Loading : HomeUiState()
     data class SuccessLoad(val boardDetail: GetBoardDetailDto) : HomeUiState()
-    data class SuccessDelete(val boardId: Unit) : HomeUiState()
+    data object SuccessDelete : HomeUiState()
     data class Error(val message: String) : HomeUiState()
 }
