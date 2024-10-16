ca689dc24f27ada73b199fcb9dc9ef8498690666
uselessNaming <seohojon@naver.com>
2024-10-16 20:57:52 +0900

    Feat: view model 연결

--- feature-mypage/src/main/java/com/materip/feature_mypage/screen/MyPage/Quiz100Screen.kt
+++ feature-mypage/src/main/java/com/materip/feature_mypage/screen/MyPage/Quiz100Screen.kt
@@ -1,8 +1,10 @@
 package com.materip.feature_mypage.screen.MyPage
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -27,46 +29,92 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.materip.core_common.ErrorState
 import com.materip.core_designsystem.component.NormalTopBar
 import com.materip.core_designsystem.component.QuizEditItem
 import com.materip.core_designsystem.component.QuizItem
 import com.materip.core_designsystem.icon.Icons
 import com.materip.core_designsystem.theme.MateTripColors
+import com.materip.core_model.request.QnARequestDto
+import com.materip.core_model.request.QnARequestItem
+import com.materip.core_model.response.QnAResponseDto
 import com.materip.core_model.ui_model.QuizClass
+import com.materip.feature_mypage.R
+import com.materip.feature_mypage.view_models.MyPage.QnAUiState
+import com.materip.feature_mypage.view_models.MyPage.QnAViewModel
+import com.materip.matetrip.component.DefaultLoadingComponent
+import com.materip.matetrip.toast.ErrorView
 
 @Composable
 fun Quiz100Route(
     navBack: () -> Unit,
+    viewModel: QnAViewModel = hiltViewModel()
 ) {
-    Quiz100Screen(navBack = navBack)
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val errState by viewModel.errorState.collectAsStateWithLifecycle()
+    Quiz100Screen(
+        uiState = uiState,
+        errState = errState,
+        deleteQuiz = viewModel::deleteQna,
+        postQuiz = viewModel::postQnA,
+        navBack = navBack
+    )
 }
 
 @Composable
-fun Quiz100Screen(navBack: () -> Unit){
-    var isEditable by remember{mutableStateOf(false)}
-    val dummyQuiz = remember{
-        mutableStateListOf(
-            QuizClass(
-                title = "고기 vs 해산물",
-                answer = ""
-            ),
-            QuizClass(
-                title = "액티비티 vs 힐링",
-                answer = ""
-            ),
-            QuizClass(
-                title = "낮 vs 밤 활동성이 많은 시간",
-                answer = ""
-            ),
-        )
+fun Quiz100Screen(
+    uiState: QnAUiState,
+    errState: ErrorState,
+    deleteQuiz: (ids: List<Int?>) -> Unit,
+    postQuiz: (List<QnARequestItem>) -> Unit,
+    navBack: () -> Unit
+){
+    when(uiState){
+        QnAUiState.Loading -> DefaultLoadingComponent()
+        QnAUiState.Error -> {
+            ErrorView(errState = errState, navBack = navBack)
+        }
+        is QnAUiState.Success -> {
+            Quiz100Content(
+                quizs = uiState.data,
+                onDeleteClick = deleteQuiz,
+                onPostQuizs = postQuiz,
+                navBack = navBack
+            )
+        }
     }
-    val removeQuiz = remember{ mutableStateListOf<QuizClass>() }
+}
+
+@Composable
+private fun Quiz100Content(
+    quizs: List<QnARequestItem>,
+    onDeleteClick: (ids: List<Int?>) -> Unit,
+    onPostQuizs: (List<QnARequestItem>) -> Unit,
+    navBack: () -> Unit,
+){
+    var isEditable by remember{mutableStateOf(false)}
+    val quizs = remember{mutableStateListOf(*quizs.toTypedArray())}
+    val removeQuiz = remember{ mutableStateListOf<Int?>() }
+
+    BackHandler(
+        enabled = true,
+        onBack = {
+            onPostQuizs(quizs)
+            navBack()
+        }
+    )
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -81,7 +129,9 @@ fun Quiz100Screen(navBack: () -> Unit){
             onBackClick = navBack,
             onClick = {
                 if(isEditable){
-                    dummyQuiz.removeAll(removeQuiz)
+                    quizs.filter{it.id !in removeQuiz}
+                    onDeleteClick(removeQuiz)
+                    removeQuiz.clear()
                 }
                 isEditable = !isEditable
             }
@@ -89,14 +139,14 @@ fun Quiz100Screen(navBack: () -> Unit){
         Spacer(Modifier.height(10.dp))
         if (isEditable){
             EditableQuiz(
-                dummyQuiz = dummyQuiz,
+                quizs = quizs,
                 removeQuiz = removeQuiz,
-                addAll = {removeQuiz.addAll(dummyQuiz)}
+                addAll = {removeQuiz.addAll(quizs.map{it.id})}
             )
         } else {
             ReadOnlyQuiz(
-                dummyQuiz = dummyQuiz,
-                addDummyQuiz = {dummyQuiz.add(it)}
+                quizs = quizs,
+                addQuiz = {quizs.add(it)}
             )
         }
     }
@@ -104,10 +154,11 @@ fun Quiz100Screen(navBack: () -> Unit){
 
 @Composable
 private fun EditableQuiz(
-    dummyQuiz: SnapshotStateList<QuizClass>,
-    removeQuiz: SnapshotStateList<QuizClass>,
+    quizs: SnapshotStateList<QnARequestItem>,
+    removeQuiz: SnapshotStateList<Int?>,
     addAll: () -> Unit,
 ){
+    val size by remember{mutableStateOf(quizs.size)}
     Column(
         modifier = Modifier.fillMaxWidth()
     ){
@@ -117,7 +168,7 @@ private fun EditableQuiz(
             verticalAlignment = Alignment.CenterVertically
         ){
             Text(
-                text = "총 ${dummyQuiz.size}개",
+                text = "총 ${size}개",
                 fontSize = 16.sp,
                 fontFamily = FontFamily(Font(com.materip.core_designsystem.R.font.noto_sans_kr)),
                 fontWeight = FontWeight(500)
@@ -132,18 +183,49 @@ private fun EditableQuiz(
             )
         }
         Spacer(Modifier.height(20.dp))
-        LazyColumn(
-            verticalArrangement = Arrangement.spacedBy(12.dp)
-        ){
-            itemsIndexed(dummyQuiz){idx, quiz ->
-                QuizEditItem(
-                    indexNumber = idx + 1,
-                    title = quiz.title,
-                    isClicked = quiz in removeQuiz,
-                    onClick = {
-                        if(quiz in removeQuiz){removeQuiz.remove(quiz)}
-                        else {removeQuiz.add(quiz)}
-                    }
+        if (quizs.isNotEmpty()){
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(12.dp)
+            ){
+                itemsIndexed(quizs){idx, quiz ->
+                    val isClicked = quiz.id in removeQuiz
+                    QuizEditItem(
+                        indexNumber = idx + 1,
+                        title = quiz.questions,
+                        isClicked = isClicked,
+                        onClick = {
+                            if(isClicked){removeQuiz.remove(quiz.id)}
+                            else { if(quiz.id != null) {removeQuiz.add(quiz.id!!)} }
+                        }
+                    )
+                }
+            }
+        } else {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ){
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = "나만의 백문백답을 만들고",
+                    style = TextStyle(
+                        fontFamily = FontFamily(Font(com.materip.core_designsystem.R.font.noto_sans_kr)),
+                        fontSize = 16.sp,
+                        color = MateTripColors.Gray_04,
+                    ),
+                    textAlign = TextAlign.Center
+                )
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = "나와 비슷한 동행자를 찾아 보세요!",
+                    style = TextStyle(
+                        fontFamily = FontFamily(Font(com.materip.core_designsystem.R.font.noto_sans_kr)),
+                        fontSize = 16.sp,
+                        color = MateTripColors.Gray_04,
+                        platformStyle = PlatformTextStyle(includeFontPadding = false)
+                    ),
+                    textAlign = TextAlign.Center
                 )
             }
         }
@@ -152,9 +234,10 @@ private fun EditableQuiz(
 
 @Composable
 private fun ReadOnlyQuiz(
-    dummyQuiz: List<QuizClass>,
-    addDummyQuiz: (QuizClass) -> Unit,
+    quizs: List<QnARequestItem>,
+    addQuiz: (QnARequestItem) -> Unit,
 ){
+    val size by remember{ mutableStateOf(quizs.size) }
     Column(
         modifier = Modifier.fillMaxWidth()
     ){
@@ -164,14 +247,14 @@ private fun ReadOnlyQuiz(
             verticalAlignment = Alignment.CenterVertically
         ){
             Text(
-                text = "총 ${dummyQuiz.size}개",
+                text = "총 ${size}개",
                 fontSize = 16.sp,
                 fontFamily = FontFamily(Font(com.materip.core_designsystem.R.font.noto_sans_kr)),
                 fontWeight = FontWeight(500)
             )
             IconButton(
                 modifier = Modifier.size(24.dp),
-                onClick = { addDummyQuiz(QuizClass("","")) }
+                onClick = { addQuiz(QnARequestItem(id = null, questions = "", answers = "")) }
             ){
                 Icon(
                     modifier = Modifier.fillMaxSize(),
@@ -181,16 +264,46 @@ private fun ReadOnlyQuiz(
             }
         }
         Spacer(Modifier.height(20.dp))
-        LazyColumn(
-            verticalArrangement = Arrangement.spacedBy(12.dp)
-        ){
-            itemsIndexed(dummyQuiz){idx, quiz ->
-                QuizItem(
-                    indexNumber = idx + 1,
-                    title = quiz.title,
-                    onUpdateTitle = { quiz.title = it },
-                    answer = quiz.answer,
-                    onUpdateAnswer = {quiz.answer = it}
+        if(quizs.isNotEmpty()){
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(12.dp)
+            ){
+                itemsIndexed(quizs){idx, quiz ->
+                    QuizItem(
+                        indexNumber = idx + 1,
+                        title = quiz.questions,
+                        onUpdateTitle = { quiz.questions = it },
+                        answer = quiz.answers,
+                        onUpdateAnswer = {quiz.answers = it}
+                    )
+                }
+            }
+        } else {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ){
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = "나만의 백문백답을 만들고",
+                    style = TextStyle(
+                        fontFamily = FontFamily(Font(com.materip.core_designsystem.R.font.noto_sans_kr)),
+                        fontSize = 16.sp,
+                        color = MateTripColors.Gray_04,
+                    ),
+                    textAlign = TextAlign.Center
+                )
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = "나와 비슷한 동행자를 찾아 보세요!",
+                    style = TextStyle(
+                        fontFamily = FontFamily(Font(com.materip.core_designsystem.R.font.noto_sans_kr)),
+                        fontSize = 16.sp,
+                        color = MateTripColors.Gray_04,
+                        platformStyle = PlatformTextStyle(includeFontPadding = false)
+                    ),
+                    textAlign = TextAlign.Center
                 )
             }
         }
@@ -200,5 +313,27 @@ private fun ReadOnlyQuiz(
 @Preview
 @Composable
 private fun Quiz100UITest(){
-    Quiz100Screen(navBack = {})
+    val quizs = listOf(
+        QnARequestItem(
+            questions = "고기 vs 해물",
+            answers = "",
+            id = null,
+        ),
+        QnARequestItem(
+            questions = "액티비티 vs 힐링",
+            answers = "",
+            id = null,
+        ),
+        QnARequestItem(
+            questions = "낮 vs 밤 활동성이 많은 시간",
+            answers = "",
+            id = null,
+        )
+    )
+    Quiz100Content(
+        quizs = quizs,
+        onDeleteClick = {},
+        onPostQuizs = {},
+        navBack = {}
+    )
 }
