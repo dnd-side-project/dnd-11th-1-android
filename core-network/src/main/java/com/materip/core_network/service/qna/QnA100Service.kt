67266335f699116480b05dcbba74f1260ef95403
uselessNaming <seohojon@naver.com>
2024-10-16 20:56:37 +0900

    Feat: 백문백답 service 추가

--- core-network/src/main/java/com/materip/core_network/service/qna/QnA100Service.kt
+++ core-network/src/main/java/com/materip/core_network/service/qna/QnA100Service.kt
@@ -3,6 +3,7 @@ package com.materip.core_network.service.qna
 import com.materip.core_model.accompany_board.all.BoardListResponse
 import com.materip.core_model.request.DefaultIdsRequestDto
 import com.materip.core_model.request.PagingRequestDto
+import com.materip.core_model.request.PagingRequestIntDto
 import com.materip.core_model.request.QnARequestDto
 import com.materip.core_model.response.DefaultPagingResponseDto
 import com.materip.core_model.response.QnAResponseDto
@@ -18,7 +19,7 @@ interface QnA100Service {
     ): ApiResponse<Any>
     @POST("/api/v1/qna100s/all")
     suspend fun getQnA(
-        @Body requestDto: PagingRequestDto
+        @Body requestDto: PagingRequestIntDto
     ): ApiResponse<DefaultPagingResponseDto<QnAResponseDto>>
     @DELETE("/api/v1/qna100s")
     suspend fun deleteQnA(
