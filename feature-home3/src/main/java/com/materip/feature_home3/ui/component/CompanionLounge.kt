56c215395ffe9d5917ac689d7f6261bba4d66156
상은 <71699054+vmkmym@users.noreply.github.com>
2024-11-11 17:04:43 +0900

    feat: 동행라운지 필터링 로직 추가

--- feature-home3/src/main/java/com/materip/feature_home3/ui/component/CompanionLounge.kt
+++ feature-home3/src/main/java/com/materip/feature_home3/ui/component/CompanionLounge.kt
@@ -10,18 +10,31 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.materip.core_designsystem.component.RegionTag
 import com.materip.core_designsystem.icon.Icons.filter_icon
+import com.materip.core_designsystem.theme.MateTripColors.Blue_02
 import com.materip.core_designsystem.theme.MateTripTypographySet
+import com.materip.feature_home3.viewModel.BoardViewModel
 
 @Composable
 fun CompanionLounge(
-    onRegionSelected: (String) -> Unit
+    onRegionSelected: (String) -> Unit,
+    viewModel: BoardViewModel = hiltViewModel()
 ) {
+    var showFilterDialog by remember { mutableStateOf(false) }
+    val selectedOption by viewModel.selectedOption.collectAsStateWithLifecycle()
+
     Column(
         modifier = Modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.Start,
@@ -38,12 +51,13 @@ fun CompanionLounge(
                 style = MateTripTypographySet.headline06,
             )
             IconButton(
-                onClick = { /*TODO: 게시글이 오늘 날짜를 기준으로 유효한지 안한지에 대해 필터링할 수 있는 아이콘*/ },
+                onClick = { showFilterDialog = true },
             ) {
                 Icon(
                     painter = painterResource(id = filter_icon),
                     contentDescription = "filter icon",
-                    modifier = Modifier.size(18.dp)
+                    modifier = Modifier.size(18.dp),
+                    tint = if (selectedOption == "전체") Blue_02 else Color.Black
                 )
             }
         }
@@ -51,5 +65,15 @@ fun CompanionLounge(
             onClick = onRegionSelected,
             modifier = Modifier.fillMaxWidth()
         )
+        if (showFilterDialog) {
+            ShowPostFilter(
+                onDismissRequest = { showFilterDialog = false },
+                onFilterSelected = { region, started, recruited ->
+                    viewModel.updateFilter(region, started, recruited)
+                },
+                selectedOption = selectedOption,
+                onOptionSelected = { option -> viewModel.updateSelectedOption(option) }
+            )
+        }
     }
 }
