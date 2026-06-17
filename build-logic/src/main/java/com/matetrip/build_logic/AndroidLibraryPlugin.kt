a4e4763b0b123a389b575ab2dac0e059d92c6514
uselessNaming <seohojon@naver.com>
2026-06-17 17:25:03 +0900

    fix: 함수 호출 타입 변경에 따른 업데이트

--- build-logic/src/main/java/com/matetrip/build_logic/AndroidLibraryPlugin.kt
+++ build-logic/src/main/java/com/matetrip/build_logic/AndroidLibraryPlugin.kt
@@ -1,10 +1,8 @@
 package com.matetrip.build_logic
 
-import com.android.build.api.dsl.LibraryExtension
 import com.matetrip.build_logic.extensions.configureAndroid
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.getByType
 
 class AndroidLibraryPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -12,7 +10,7 @@ class AndroidLibraryPlugin : Plugin<Project> {
             pluginManager.apply("com.android.library")
             pluginManager.apply("org.jetbrains.kotlin.android")
 
-            extensions.getByType<LibraryExtension>().configureAndroid()
+            configureAndroid()
         }
     }
 }
